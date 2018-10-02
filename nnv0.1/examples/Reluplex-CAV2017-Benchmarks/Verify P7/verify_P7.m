5c078aecb9df4c5c25d7cf3746986ad67db138ce
trhoangdung <trhoangdung@gmail.com>
2018-10-02 12:42:46 -0500

    normalize input for P7 Reluplex case study

--- nnv0.1/examples/Reluplex-CAV2017-Benchmarks/Verify P7/verify_P7.m
+++ nnv0.1/examples/Reluplex-CAV2017-Benchmarks/Verify P7/verify_P7.m
@@ -23,6 +23,13 @@ F = FFNN(Layers);
 
 lb = [0; -3.141592; -3.141592; 100; 0];
 ub = [60760; 3.141592; 3.141592; 1200; 1200];
+
+% normalize input
+for i=1:5
+    lb(i) = (lb(i) - means_for_scaling(i))/range_for_scaling(i);
+    ub(i) = (ub(i) - means_for_scaling(i))/range_for_scaling(i);   
+end
+
 I = Polyhedron('lb', lb, 'ub', ub);
 
 

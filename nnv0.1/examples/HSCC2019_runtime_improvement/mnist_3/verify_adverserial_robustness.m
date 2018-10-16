0d46b639d466111291cf5ffa91ad7d32738392c3
trhoangdung <trhoangdung@gmail.com>
2018-10-16 10:23:11 -0500

    modify runtime reduction example

--- nnv0.1/examples/HSCC2019_runtime_improvement/mnist_3/verify_adverserial_robustness.m
+++ nnv0.1/examples/HSCC2019_runtime_improvement/mnist_3/verify_adverserial_robustness.m
@@ -1,22 +1,19 @@
-load mnist5x50.mat;
+load mnist2x500.mat;
 load ones.mat;
 
 Layers = [];
-for i=1:6
-    W = mnist_5x50.W{1, i};
-    b = mnist_5x50.b{1,i}';
+for i=1:3
+    W = mnist2x250.W{1, i};
+    b = mnist2x250.b{1,i}';
     L = Layer(W, b, 'ReLU');
     Layers = [Layers L];
 end
 
 F = FFNN(Layers);
 
-% we perform the test on 100 images of each digit
-n_inputs = size(ones, 1); % number of digit 1's images in the test: 100
+attacked_pos = [10 11 12 13 14 15 16];
 
-attacked_pos = [1 2 3 4 5 6];
-
-bound = 0.2; % bound of disturbance
+bound = 0.6; % bound of disturbance
 
 input_vec = ones(1, :)';
 lb = input_vec;
@@ -35,16 +32,45 @@ end
 V = Reduction.getVertices(lb, ub);
 I = Polyhedron('V', V');
 
-n_cores = [4];
-runtime = [];
-output = []; 
+n_cores = [1, 2];
 
-% exact-scheme
+runtime1 = [];
+output1 = [];
+
+% lazy-approximate scheme
 for i=1:length(n_cores)
-    [R, t] = F.reach(I, 'exact', n_cores(i), []);
+    [R, t] = F.reach(I, 'approx', n_cores(i), []);
     R1 = Reduction.hypercubeHull(R);
     range = [R1.lb R1.ub];
-    runtime = [runtime t];
-    output = vertcat(output, range);
+    runtime1 = [runtime1 t];
+    output1 = vertcat(output1, range);
 end
 
+% lazy-approximate scheme + input partition
+I1 = Partition.partition_box(Box(lb, ub), 1);
+runtime2 = [];
+output2 = [];
+for i=1:length(n_cores)
+    [R, t] = F.reach(I1, 'approx', n_cores(i), []);
+    R1 = Reduction.hypercubeHull(R);
+    range = [R1.lb R1.ub];
+    runtime2 = [runtime2 t];
+    output2 = vertcat(output2, range);
+end
+
+
+reduction1 = [];
+reduction2 = [];
+
+
+for i=1:2
+    reduction1 = [reduction1 (runtime1(1) - runtime1(i))/runtime1(1)];
+end
+
+
+for i=1:2
+    reduction2 = [reduction2 (runtime2(1) - runtime2(i))/runtime2(1)];
+end
+
+
+save result.mat;

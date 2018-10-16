99250e444d056eedce1ebccd594b350954e9f95d
trhoangdung <trhoangdung@gmail.com>
2018-10-16 12:46:14 -0500

    add new networks for P2

---
+++ nnv0.1/examples/Reluplex-CAV2017-Benchmarks/Verify P2/On N2_2/verify_P2.m
@@ -0,0 +1,39 @@
+load ACASXU_run2a_2_2_batch_2000.mat;
+Layers = [];
+n = length(b);
+for i=1:n - 1
+    bi = cell2mat(b(i));
+    Wi = cell2mat(W(i));
+    Li = Layer(Wi, bi, 'ReLU');
+    Layers = [Layers Li];
+end
+bn = cell2mat(b(n));
+Wn = cell2mat(W(n));
+Ln = Layer(Wn, bn, 'Linear');
+
+Layers = [Layers Ln];
+F = FFNN(Layers);
+
+% Input Constraints
+% 55947.69 <= i1(\rho) <= 60760,
+% -3.14 <= i2 (\theta) <= 3.14,
+%-3.14 <= i3 (\shi) <= -3.14
+% 1145 <= i4 (\v_own) <= 1200, 
+% 0 <= i5 (\v_in) <= 60
+
+lb = [55947.69; -3.14; -3.14; 1145; 0];
+ub = [60760; 3.14; 3.14; 1200; 60];
+
+% normalize input
+for i=1:5
+    lb(i) = (lb(i) - means_for_scaling(i))/range_for_scaling(i);
+    ub(i) = (ub(i) - means_for_scaling(i))/range_for_scaling(i);   
+end
+
+V = Reduction.getVertices(lb, ub);
+
+I = Polyhedron('V', V');
+
+[R, t] = F.reach(I, 'exact', 4, []); % exact reach set
+save result.mat % save the verified network
+F.print('F.info'); % print all information to a file

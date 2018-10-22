805948e4174d497842a8abe3b7f5115c0040b569
trhoangdung <trhoangdung@gmail.com>
2018-10-22 14:42:18 -0500

    reorganized nnv

---
+++ nnv0.1/engine/nncs/NNCS.m
@@ -0,0 +1,15 @@
+classdef NNCS
+    %Neural network-based control system class
+    %   Dung Tran: 10/21/2018
+    
+    properties
+        controller = []; % nerual-network controller
+        plant = []; % plant model, could be linear, nonlinear or neural network-based plant
+        k = 0; % sampling period
+    end
+    
+    methods
+    end
+    
+end
+

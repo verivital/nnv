29a1eb9a24001ba194e37139e7b10bb34a07e987
trhoangdung <trhoangdung@gmail.com>
2018-10-24 13:00:38 -0500

    add simDirect, simKrylov and simOde45 methods in LinearODE class for simulation-based reachability analysis

--- nnv0.1/engine/nncs/LinearODE.m
+++ nnv0.1/engine/nncs/LinearODE.m
@@ -15,66 +15,46 @@ classdef LinearODE
     methods
         
         % constructor
-        function obj = LinearODE(A, B, C, D)
+        function obj = LinearODE(A, B, C, D)       
             
-            
-            if nargin < 3
-                error('Missing input matrices to construct a linearODE system');
-            elseif nargin == 3
+            if isempty(A)
+                error('Matrix A is empty');
+            else 
                 [nA, mA] = size(A);
-                [nB, mB] = size(B);
-                [nC, mC] = size(C);
-
-
                 if nA ~= mA
                     error('A should be a square matrix');
                 end
-                if nA ~= nB
-                    error('A and B are inconsistent');
-                end
-                if mC ~= nA
-                    eror('A and C are inconsistent');
-                end
-
-                obj.A = A;
-                obj.B = B;
-                obj.C = C;
-                obj.nI = mB;
-                obj.nO = nC;
-                obj.dim = nA;
-
-            elseif nargin == 4
-                
-                [nA, mA] = size(A);
+            end
+            
+            if ~isempty(B)
                 [nB, mB] = size(B);
-                [nC, mC] = size(C);
-                [nD, ~] = size(D);
-                
-                if nA ~= mA
-                    error('A should be a square matrix');
-                end
                 if nA ~= nB
                     error('A and B are inconsistent');
                 end
+            end
+            
+            if ~isempty(C)
+                [nC, mC] = size(C);
                 if mC ~= nA
                     error('A and C are inconsistent');
                 end
-                                
+            end
+            
+            if ~isempty(C) && ~isempty(D)
+               [nD, ~] = size(D); 
+
                 if nD ~= nC
                     error('C and D are inconsistent');
                 end
-
-                obj.A = A;
-                obj.B = B;
-                obj.C = C;
-                obj.D = D;
-                obj.nI = mB;
-                obj.nO = nC;
-                obj.dim = nA;
-                
-            elseif nargin > 4
-                error('Too many inputs');
             end
+
+            obj.A = A;
+            obj.B = B;
+            obj.C = C;
+            obj.D = D;
+            obj.nI = mB;
+            obj.nO = nC;
+            obj.dim = nA;
                 
         end
         
@@ -124,8 +104,7 @@ classdef LinearODE
             
         end
         
-        % convert to discrete ODE
-        
+        % convert to discrete ODE       
         function sysd = c2d(obj, Ts)
             % @Ts: sampling time
             % @sysd: discrete linear ODE
@@ -137,8 +116,128 @@ classdef LinearODE
             sysd = DLinearODE(sys1.A, sys1.B, sys1.C, sys1.D, Ts);
             
         end
+                
+    end
+    
+    methods(Static)
+    
+        % simulation of dot{x} = Ax using direct method
+        function X = simDirect(A, x0, h, N)
+            % @A: system matrix
+            % @x0: initial states
+            % @h: timeStep for simulation
+            % @N: number of steps
+            % @X : simulation result X = [x0, x1, ..., xN]; 
+            % author: Dung Tran 
+            % date : May/30/2017
+            
+            n = size(A,1);
+
+            E = expm(h*A);
+            X = zeros(n,N);
+            x01 = x0;
+            for i=1:N
+                X(:,i) = E*x01;
+                x01 = X(:,i);
+            end
+            
+            X = [x0 X];
+            
+        end
         
+        % simulation of dot{x} = Ax using Krylov subspace method
+        function X = simKrylov(A, x0, h, N, m)
+            % @A : the input matrix
+            % @x0 : the initial state vector 
+            % @h: time Step for simulation
+            % @N: number of Step we want to simulate
+            % @m : the number of basic vectors for Krylov supspace Km, Vm = [v1, .., vm]
+
+            % @X : simulation result X = [x0, x1, ..., xN]; 
+            % author: Dung Tran 
+            % date : May/30/2017
+            
+            function [Q,H] = arnoldi(A,q1,m)
+                %ARNOLDI    Arnoldi iteration
+                %   [Q,H] = ARNOLDI(A,q1,M) carries out M iterations of the
+                %   Arnoldi iteration with N-by-N matrix A and starting vector q1
+                %   (which need not have unit 2-norm).  For M < N it produces
+                %   an N-by-(M+1) matrix Q with orthonormal columns and an
+                %   (M+1)-by-M upper Hessenberg matrix H such that
+                %   A*Q(:,1:M) = Q(:,1:M)*H(1:M,1:M) + H(M+1,M)*Q(:,M+1)*E_M',
+                %   where E_M is the M'th column of the M-by-M identity matrix.
+
+                n = length(A);
+                if nargin < 3, m = n; end
+                q1 = q1/norm(q1);
+                Q = zeros(n,m); Q(:,1) = q1;
+                H = zeros(min(m+1,m),n);
+
+                for k=1:m
+                    z = A*Q(:,k);
+                    for i=1:k
+                        H(i,k) = Q(:,i)'*z;
+                        z = z - H(i,k)*Q(:,i);
+                    end
+                    if k < n
+                       H(k+1,k) = norm(z);
+                       if H(k+1,k) == 0, return, end
+                       Q(:,k+1) = z/H(k+1,k);
+                   end
+                end
+            end
+
+
+            [mA] = size(A,1);
+            % Krylov supspace method
+            Im = eye(m);
+            e1 = Im(:,1); % 1fst unit vector of R^m 
+            X = zeros(mA,N);
+            [V,H] = arnoldi(A,x0,m);
+            beta  = norm(x0);
+            Vm = V(:,1:m);
+            Hm = H(1:m,1:m);
+            Vmm = beta*Vm;
+            Hms = h*Hm;
+
+            for i=1:N
+                P = expm(i*Hms);
+                X(:,i) = Vmm*P*e1;
+            end
+            
+            X = [x0 X];
+                   
+        end
         
+        % simulation of dot{x} = Ax using ode45
+        function X = simOde45(A, x0, h, N)
+            % @A: system matrix
+            % @x0: initial states
+            % @h: timeStep for simulation
+            % @N: number of steps
+            % @X : simulation result X = [x0, x1, ..., xN]; 
+            
+            % author: Dung Tran 
+            % date : May/30/2017
+            
+            function dydt = odefun(y,A)
+                %This function generate ode function from matrix A
+                [mA,nA] = size(A);
+                dydt = zeros(mA,1);
+                for i=1:mA
+                    for j=1:nA
+                        dydt(i)= dydt(i) + A(i,j)*y(j);
+                    end
+                end
+            end
+            
+            timeSpan = 0:h:h*N;
+            [~,y] = ode45(@(t,y) odefun(y,A), timeSpan, x0);
+            X = y';
+            
+        end
+        
+              
     end
     
 end

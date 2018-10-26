e11646302376bd52855889a65dea8cc733f87649
trhoangdung <trhoangdung@gmail.com>
2018-10-26 16:19:59 -0500

    add convexHull with linear Transform method in zono class

---
+++ nnv0.1/tests/set_tests/test_zono_convexHull_with_linearTransform.m
@@ -0,0 +1,29 @@
+c = [1; 1];
+V = [1 0 1; 1 1 0];
+Z1 = Zono(c, V);
+
+W = [2 1; 0 -1];
+b = [];
+
+Z2 = Z1.affineMap(W, b);
+
+Z12 = Z1.convexHull(Z2);
+
+
+figure;
+Z12.plot;
+hold on;
+Z1.plot;
+hold on;
+Z2.plot;
+
+Z3 = Z1.convexHull_with_linearTransform(W);
+figure;
+Z3.plot;
+hold on;
+Z1.plot;
+hold on;
+Z2.plot;
+
+
+

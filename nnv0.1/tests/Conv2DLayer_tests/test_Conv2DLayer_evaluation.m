0e5dbb5c5b855a40a3c982824122771af2e81697
trhoangdung <trhoangdung@gmail.com>
2018-12-11 06:41:42 -0600

    add compute_featureMap method for Conv2DLayer and test case

---
+++ nnv0.1/tests/Conv2DLayer_tests/test_Conv2DLayer_evaluation.m
@@ -0,0 +1,48 @@
+% construct a Conv2DLayer object
+% this convolution layer is from the link: 
+% http://cs231n.github.io/convolutional-networks/
+
+L = Conv2DLayer(3, 1);
+L.set_name('Test_Convolutional_Layer');
+
+% filter 1 weight matrix with 3 channels
+W(:,:, 1, 1) = [1 1 1; 1 1 -1; 0 -1 0]; % channel 1
+W(:, :, 2, 1) = [0 1 1; 0 -1 0; 0 0 -1]; % channel 2
+W(:, :, 3, 1) = [1 -1 0; 1 1 0; -1 0 1]; % channel 3
+% filter 2 weight matrix with 3 channels
+W(:, :, 1, 2) = [1 1 -1; -1 0 1; 1 -1 -1]; % channel 1
+W(:, :, 2, 2) = [-1 1 1; 0 1 1; 0 -1 1]; % channel 2
+W(:, :, 3, 2) = [-1 1 -1; -1 0 -1; -1 -1 -1]; % channel 3
+
+% biases for 2 filters
+b(:, :, 1) = 1; % filter 1
+b(:, :, 2) = 0; % filter 2
+
+
+L.set_weights_biases(W, b);
+
+
+% the convolutional layer has 2 filters of size 3x3
+padding = 1;
+stride = 2;
+dilation = 1;
+
+L.set_stride(stride);
+L.set_padding(padding);
+L.set_dilation(dilation);
+
+% original input volume: color image with 3 channels
+inputVol(:, :, 1) = [2 0 1 2 1; 1 0 2 2 2; 1 2 2 0 2; 1 2 0 0 1; 1 0 1 1 2]; % channel 1 input matrix
+inputVol(:, :, 2) = [0 0 1 0 1; 0 0 2 1 1; 1 1 0 1 1; 1 1 0 2 2; 2 1 2 0 0]; % channel 2 input matrix
+inputVol(:, :, 3) = [1 2 2 1 0; 2 0 0 2 0; 0 0 1 0 1; 1 2 0 2 0; 1 0 2 1 0]; % channel 3 input matrix
+
+
+% construct input with padding operation
+paddingSize = [1 1 1 1];
+I = Conv2DLayer.get_input(inputVol, paddingSize);
+
+
+
+
+
+

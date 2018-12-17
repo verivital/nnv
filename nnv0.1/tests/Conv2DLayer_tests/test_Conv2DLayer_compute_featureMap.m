c2149d15cc748e89ac53707d0529b80ec8dc29ca
trhoangdung <trhoangdung@gmail.com>
2018-12-17 13:38:21 -0600

    finish evaluation for Convolutional 2d Layer, add tests

---
+++ nnv0.1/tests/Conv2DLayer_tests/test_Conv2DLayer_compute_featureMap.m
@@ -0,0 +1,13 @@
+% this example is from Figure 5 in the website:
+% https://ujjwalkarn.me/2016/08/11/intuitive-explanation-convnets/
+
+I = [1 1 1 0 0; 0 1 1 1 0; 0 0 1 1 1; 0 0 1 1 0; 0 1 1 0 0]; % input
+W = [1 0 1; 0 1 0; 1 0 1]; % fiter
+
+stride = [1 1];
+dilation = [1 1];
+
+
+featureMap = Conv2DLayer.compute_featureMap(I, W, stride, dilation);
+
+

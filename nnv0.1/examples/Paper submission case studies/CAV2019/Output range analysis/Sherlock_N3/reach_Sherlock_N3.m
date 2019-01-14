7d264534e78d7ee327935fd7128712096e82cf87
trhoangdung <trhoangdung@gmail.com>
2019-01-14 16:36:20 -0600

    add output range analysis case study to CAV2019

---
+++ nnv0.1/examples/Paper submission case studies/CAV2019/Output range analysis/Sherlock_N3/reach_Sherlock_N3.m
@@ -0,0 +1,22 @@
+load neural_network_information_3.mat
+n = length(W);
+L = [];
+for i=1:n
+    L1 = Layer(W{1, i}, b{1, i}, 'ReLU');
+    L = [L L1];
+end
+
+F = FFNN(L); % neural network
+
+lb = [0; 0];
+ub = [10; 10];
+
+I = Box(lb, ub); % input set = Sherlock
+t = tic;
+[R1, t1] = F.reach(I.toStar, 'exact', 4, []);
+exact_range = Star.get_hypercube_hull(R1);
+reach_time = toc(t);
+save exact_range exact_range;
+save reach_time reach_time;
+save outputSet.mat R1; % save the verified network
+F.print('F.info'); % print all information to a file

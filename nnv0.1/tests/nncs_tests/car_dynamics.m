b7ea067da88ce96c25c678aadb9253a22b37960b
trhoangdung <trhoangdung@gmail.com>
2019-01-29 17:43:32 -0600

    add evaluate method to NNCS class and its tests, work well

--- nnv0.1/tests/nncs_tests/car_dynamics.m
+++ nnv0.1/tests/nncs_tests/car_dynamics.m
@@ -1,8 +1,23 @@
-function [dx]=car_dynamics(t,x, u)
+function [dx]=car_dynamics(t,x, a_ego)
 
-mu=0.1; % friction parameter
+mu=0.0001; % friction parameter
 
+% x1 = lead_car position
+% x2 = lead_car velocity
+% x3 = lead_car internal state
+
+% x4 = ego_car position
+% x5 = ego_car velocity
+% x6 = ego_car internal state
+
+% u = a_ego: control input
+
+% lead car dynamics
+a_lead = -2; 
 dx(1,1)=x(2);
-dx(2,1)= -0.5 * x(2)- mu * x(2)^2;
-dx(3,1) = x(4);
-dx(4,1) = -0.5 * x(4) - mu * x(4)^2  + u;
+dx(2,1) = x(3);
+dx(3,1) = -2 * x(3) + 2 * a_lead - mu*x(2)^2;
+% ego car dyanmics
+dx(4,1)= x(5); 
+dx(5,1) = x(6);
+dx(6,1) = -2 * x(6) + 2 * a_ego - mu*x(5)^2;
---
+++ nnv0.1/tests/nncs_tests/test_nncs_evaluate.m
@@ -0,0 +1,32 @@
+load controller_3_20.mat;
+weights = network.weights;
+bias = network.bias;
+n = length(weights);
+Layers = [];
+for i=1:n - 1
+    L = Layer(weights{1, i}, bias{i, 1}, 'ReLU');
+    Layers = [Layers L];
+end
+
+L = Layer(weights{1, n}, bias{n, 1}, 'Linear');
+
+Layers = [Layers L];
+
+Ts = 0.1;
+
+Controller = FFNN(Layers); % feedforward neural network controller
+Plant = NonLinearODE(6, 1, @car_dynamics);
+Plant.set_timeStep(0.01); % time step for reachability analysis of the plant
+Plant.set_tFinal(Ts); % Ts = 0.1, sampling time for control signal from neural network controller
+output_mat = [0 0 0 0 1 0;1 0 0 -1 0 0; 0 1 0 0 -1 0]; % feedback: relative distance, relative velocity and ego-car velocity
+Plant.set_output_mat(output_mat); % Define the outputs that is feedback to the controller
+
+feedbackMap = [0]; % feedback map, y[k] 
+
+ncs = NNCS(Controller, Plant, feedbackMap); % the neural network control system
+
+x0 = [98; 32; 0; 10; 30; 0];
+ref_input = [30; 1.4];
+
+N = 50;
+simTrace = ncs.evaluate(Ts, N, x0, ref_input);

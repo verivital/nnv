e682a79706551ec70316db69ac99988ba088dea3
trhoangdung <trhoangdung@gmail.com>
2019-01-31 15:10:43 -0600

    add falsify method in NNCS class and its test, work ok

--- nnv0.1/tests/nncs_tests/test_nncs_falsify.m
+++ nnv0.1/tests/nncs_tests/test_nncs_falsify.m
@@ -12,12 +12,12 @@ L = Layer(weights{1, n}, bias{n, 1}, 'Linear');
 
 Layers = [Layers L];
 
-Ts = 0.1;
+Ts = 0.1; % control period
 
 Controller = FFNN(Layers); % feedforward neural network controller
 Plant = NonLinearODE(6, 1, @car_dynamics);
 Plant.set_timeStep(0.01); % time step for reachability analysis of the plant
-Plant.set_tFinal(Ts); % Ts = 0.1, sampling time for control signal from neural network controller
+Plant.set_tFinal(Ts); % Ts = 0.1, sampling time for control signal from neural network controller, this is also control period of the controller
 output_mat = [0 0 0 0 1 0;1 0 0 -1 0 0; 0 1 0 0 -1 0]; % feedback: relative distance, relative velocity and ego-car velocity
 Plant.set_output_mat(output_mat); % Define the outputs that is feedback to the controller
 
@@ -30,11 +30,19 @@ lb = [98; 32; 0; 10; 30; 0];
 ub = [100; 32.2; 0; 11; 30.2; 0];
 
 init_set = Box(lb, ub);
-ref_input_set = Box([30; 1.4], [30; 1.4]);
+ref_input_set = Box([30; 30], [1.4; 1.4]);
 
 n_steps = 50;
 n_samples = 50; 
 
-[sim_time, sim_traces, control_traces, sampled_init_states, sampled_ref_inputs] = ncs.sample(Ts, n_steps, init_set, ref_input_set, n_samples);
+% consider ACC case study to understand more
+t_gap = 1.4;
+D_default = 10;
 
+% safety specification dis > safe_dis
+
+unsafe_mat = [1 0 0 -1 -t_gap 0];
+unsafe_vec = [D_default];
+
+[falsify_result, falsify_time, counter_sim_traces, counter_control_traces, counter_init_states, counter_ref_inputs] = ncs.falsify(Ts, n_steps, init_set, ref_input_set, unsafe_mat, unsafe_vec, n_samples);
 

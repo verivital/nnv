caa0b70ebb421a669fbdec316a7e55dfcc9c4af4
Dung Tran <trandh@isis.vanderbilt.edu>
2019-02-01 14:30:38 -0600

    add falsify trace figure

--- nnv0.1/examples/NNCS/ACC/Verification/Scenarios 1/plot_falsify_traces.m
+++ nnv0.1/examples/NNCS/ACC/Verification/Scenarios 1/plot_falsify_traces.m
@@ -1,4 +1,5 @@
-load controller_7_20.mat;
+clear;
+load controller_3_20.mat;
 weights = network.weights;
 bias = network.bias;
 n = length(weights);
@@ -28,7 +29,7 @@ ncs = NNCS(Controller, Plant, feedbackMap); % the neural network control system
 % x = [x_lead v_lead x_internal_lead x_ego v_ego x_internal_ego]'
 
 lb = [65; 32; 0; 10; 30; 0];
-ub = [72; 32.2; 0; 11; 30.2; 0];
+ub = [70; 32.2; 0; 11; 30.2; 0];
 
 init_set = Box(lb, ub);
 
@@ -50,20 +51,45 @@ n_cores = 4; % number of cores
 unsafe_mat = [1 0 0 -1 -t_gap 0];
 unsafe_vec = [D_default];
 
-n_samples = 200; 
+n_samples = 10; 
 
 [falsify_result, falsify_time, counter_sim_traces, counter_control_traces, counter_init_states, counter_ref_inputs] = ncs.falsify(0.1, N, init_set, input_ref, unsafe_mat, unsafe_vec, n_samples);
 
 if falsify_result == 1
-    simTrace = cell2mat(counter_sim_traces{1, 1});
-    dis = [1 0 0 -1 0 0] * simTrace; 
-    safe_dis = [0 0 0 0 t_gap 0] * simTrace + D_default;
+    
+    for i=1:length(counter_sim_traces)
+        simTrace = cell2mat(counter_sim_traces{1, 1});
+        controlTrace = cell2mat(counter_control_traces{1, 1});
+        dis = [1 0 0 -1 0 0] * simTrace; 
+        safe_dis = [0 0 0 0 t_gap 0] * simTrace + D_default;
+        times = 0:0.1:0.1*N;
+        figure;
+        % subplot 1
+        subplot(3, 1, 1);
 
-    times = 0:0.1:0.1*N;
-    plot(times, dis, '-', 'Color', 'blue');
-    hold on;
-    plot(times, safe_dis, '-', 'Color', 'red');
-    xlabel('t');
-    title('Relative distance (blue) vs. safe distance');
+        plot(times, simTrace(2, :), '-', 'Color', 'green');
+        hold on;
+        plot(times, simTrace(5, :), '-', 'Color', 'blue');
+        xlabel('time (seconds)');
+        ylabel('Velocity');
+        title('Ego car velocity (blue) vs. lead car velocity (green)');
+        % subplot 2
+        subplot(3,1,2);
+        plot(times, dis, '-', 'Color', 'blue');
+        hold on;
+        plot(times, safe_dis, '-', 'Color', 'red');
+        xlabel('time (seconds)');
+        ylabel('Distance');
+        title('Relative distance (blue) vs. safe distance (red)');
+        % subplot 3
+        subplot(3,1,3);
+        plot(times(1:N), controlTrace, '-', 'Color', 'cyan');
+        xlabel('time (seconds)');
+        ylabel('Control')
+        title('Control vs. time');
+    end
+    
+
+    saveas(gcf, 'falsifyTrace.pdf');
 end
 

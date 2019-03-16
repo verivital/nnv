e9dfd5d4fa59824df16adb8d9b61716832953606
trhoangdung <trhoangdung@gmail.com>
2019-03-16 14:35:44 -0500

    add abstract-domain method to SatLin class and its test, work ok

--- nnv0.1/engine/nn/SatLin.m
+++ nnv0.1/engine/nn/SatLin.m
@@ -115,25 +115,14 @@ classdef SatLin
        
         
         
-        % function reachability analysis using Star
-        function S = reach(varargin)
+        % exact reachability analysis using Star
+        function S = reach_star_exact(I, option)
             % @I: an array of star input sets
             % @option: = 'parallel' use parallel option
             %          = '' do use parallel option
             
             % author: Dung Tran
             % date: 27/2/2019
-            
-            switch nargin
-                case 2
-                    I = varargin{1};
-                    option = varargin{2};
-                case 1
-                    I = varargin{1};
-                    option = [];
-                otherwise
-                    error('Invalid number of input arguments (should be 1 or 2)');
-            end
             
             if ~isempty(I)       
                 dim = I(1).dim;
@@ -395,15 +384,254 @@ classdef SatLin
     
     methods(Static) % reachability analysis using abstract-domain
         
-        % future supporting method
+        % step over-approximate reachability analysis using abstract-domain
+        % we extend abstract-domain method from ReLU to SatLin
+        % we use star set to represent abstract-domain
+        function S = stepReachAbstractDomain(I, index)
+            % @I: star-input set
+            % @index: index of neuron performing stepReach
+            % @S: star output set represent abstract-domain of the output
+            % set
+            
+            % author: Dung Tran
+            % date: 5/3/2019
+        
+            % reference: An Abstract Domain for Certifying Neural Networks,
+            % Gagandeep Singh, POPL 2019
+           
+            if ~isa(I, 'Star')
+                error('Input is not a Star');
+            end
+               
+            [lb, ub] = I.getRange(index);
+            
+            if ub <= 0
+                V = I.V;
+                V(index, :) = zeros(1, I.nVar + 1);
+                S = Star(V, I.C, I.d);
+            end
+            if lb >= 1
+                V = I.V;
+                V(index, :) = zeros(1, I.nVar + 1);
+                V(index, 1) = 1;
+                S = Star(V, I.C, I.d);                
+            end
+            if (1 > lb) && (lb > 0) && ub > 1
+                % constraint 1: y(index) <= x[index]
+                C1 = [-I.V(index, 2:I.nVar + 1) 1];
+                d1 = I.V(index, 1);
+                % constraint 2: y[index] <= 1
+                C2 = zeros(1, I.nVar + 1);
+                C2(I.nVar + 1) = 1;
+                d2 = 1;
+                % constraint 3: y[index] >= (1-lb)x/(ub-lb) + lb*(ub-1)/(ub-lb)
+                C3 = [((1-lb)/(ub-lb))*I.V(index, 2:I.nVar+1) -1];
+                d3 = -lb*(ub-1)/(ub-lb) - (1-lb)*I.V(index,1)/(ub-lb);
+                
+                m = size(I.C, 1);
+                C0 = [I.C zeros(m, 1)];
+                d0 = I.d;
+                new_V = [I.V zeros(I.dim, 1)];
+                new_V(index, :) = zeros(1, I.nVar+2);
+                new_V(index, I.nVar+2) = 1;
+                
+                S1 = (1-lb)*(ub-lb)/2; % area of the first candidate abstract-domain
+                S2 = (ub-lb)*(ub-1)/2;% area of the second candidate abstract-domain
+                
+                if S1 < S2
+                    % get first cadidate as resulted abstract-domain
+                    new_C = [C0; C2; C3];
+                    new_d = [d0; d2; d3];
+                else
+                    % get second candidate as resulted abstract-domain
+                    new_C = [C0; C1; C3];
+                    new_d = [d0; d1; d3];
+                end
+
+                S = Star(new_V, new_C, new_d);
+            end
+            
+            if lb >= 0 && ub <= 1
+                S = Star(I.V, I.C, I.d);
+            end
+            if lb < 0 && (0 < ub) && (ub <= 1)
+                
+                n = I.nVar + 1;
+                % over-approximation constraints 
+                % constraint 1: y[index] = >= 0
+                C1 = zeros(1, n);
+                C1(n) = -1; 
+                d1 = 0;
+                % constraint 2: y[index] >= x[index]
+                C2 = [I.V(index, 2:n) -1];
+                d2 = -I.V(index, 1);
+                % constraint 3: y[index] <= ub(x[index] -lb)/(ub - lb)
+                C3 = [-(ub/(ub-lb))*I.V(index, 2:n) 1];
+                d3 = -(ub*lb/(ub-lb))*(1 - I.V(index, 1));
+
+                m = size(I.C, 1);
+                C0 = [I.C zeros(m, 1)];
+                d0 = I.d;
+                new_V = [I.V zeros(I.dim, 1)];
+                new_V(index, :) = zeros(1, n+1);
+                new_V(index, n+1) = 1;
+                
+                
+                S1 = ub*(ub-lb)/2; % area of the first candidate abstract-domain
+                S2 = -lb*(ub-lb)/2; % area of the second candidate abstract-domain
+                
+                if S1 < S2
+                    % get first cadidate as resulted abstract-domain
+                    new_C = [C0; C1; C3];
+                    new_d = [d0; d1; d3];
+                else
+                    % get second candidate as resulted abstract-domain
+                    new_C = [C0; C2; C3];
+                    new_d = [d0; d2; d3];
+                end
+
+                S = Star(new_V, new_C, new_d);               
+            end
+            
+            if lb < 0 && ub > 1
+                % constraint 1: y[index] >= 0
+                n = I.nVar + 1;
+                C1 = zeros(1, n);
+                C1(n) = -1; 
+                d1 = 0;
+                % constraint 2: y[index] <= 1
+                C2 = zeros(1, n);
+                C2(n) = 1;
+                d2 = 1;
+                % constraint 3: y[index] <= x/(1 -lb) - lb/(1-lb)
+                C3 = [(-1/(1-lb))*I.V(index, 2:n) 1];
+                d3 = (1/(1-lb))*I.V(index, 1) - lb/(1-lb);
+                % constraint 4: y[index] >=  x/ub
+                C4 = [(1/ub)*I.V(index, 2:n) -1];
+                d4 = -(1/ub)*I.V(index, 1);
+                
+                m = size(I.C, 1);
+                C0 = [I.C zeros(m, 1)];
+                d0 = I.d;
+                new_V = [I.V zeros(I.dim, 1)];
+                new_V(index, :) = zeros(1, n+1);
+                new_V(index, n+1) = 1;
+                
+                S1 = (ub-lb)*(ub-lb)/(2*(1-lb)); % area of the first candidate abstract domain
+                S2 = (1 - lb/ub)*(ub-lb)/2; % area of the second candidate abstract domain
+                
+                if S1 < S2
+                    % get first cadidate as resulted abstract-domain
+                    new_C = [C0; C1; C3];
+                    new_d = [d0; d1; d3];
+                else
+                    % get second candidate as resulted abstract-domain
+                    new_C = [C0; C2; C4];
+                    new_d = [d0; d2; d4];
+                end
+                
+                S = Star(new_V, new_C, new_d); 
+                
+            end
+                       
+        end
+        
+        
+        % over-approximate reachability analysis using abstract-domain
+        function S = reach_abstract_domain(I)
+            % @I: star input set
+            % @S: star output set
+
+            % author: Dung Tran
+            % date: 4/3/2019
+
+
+            if ~isa(I, 'Star')
+                error('Input is not a star');
+            end
+
+            if isEmptySet(I)
+                S = [];
+            else
+                In = I;
+                for i=1:I.dim
+                    fprintf('\nPerforming PosLin_%d operation', i);
+                    In = PosLin.stepReachAbstractDomain(In, i);
+                end
+                S = In;
+            end
+
+        end     
         
     end
+    
     
     methods(Static) % reachability analysis method using face-latice
         
         % future supporting method
         
     end
+    
+    
+    methods(Static) % main reach method
+        
+        % main function for reachability analysis
+        function R = reach(varargin)
+            % @I: an array of star input sets
+            % @method: 'exact-star' or 'approx-star' or 'approx-zono' or
+            % 'abs-dom' or 'face-latice'
+            % @option: = 'parallel' use parallel option
+            %          = '' do use parallel option
+            
+            % author: Dung Tran
+            % date: 27/2/2019
+            
+            switch nargin
+                
+                case 3
+                    I = varargin{1};
+                    method = varargin{2};
+                    option = varargin{3};
+                
+                case 2
+                    I = varargin{1};
+                    method = varargin{2};
+                    option = [];
+                case 1
+                    I = varargin{1};
+                    method = 'exact-star';
+                    option = [];
+                otherwise
+                    error('Invalid number of input arguments (should be 1, 2 or 3)');
+            end
+            
+            
+            if strcmp(method, 'exact-star') % exact analysis using star
+                
+                R = SatLin.reach_star_exact(I, option);
+                
+            elseif strcmp(method, 'approx-star')  % over-approximate analysis using star
+                
+                R = SatLin.reach_star_approx(I);
+                
+            elseif strcmp(method, 'approx-zono')  % over-approximate analysis using zonotope
+                
+                R = SatLin.reach_zono_approx(I);
+                
+            elseif strcmp(method, 'abs-dom')  % over-approximate analysis using abstract-domain
+                
+                R = SatLin.reach_abstract_domain(I);
+                
+            elseif strcmp(method, 'exact-face-latice') % exact analysis using face-latice
+                fprintf('\nNNV have not yet support Exact Face-Latice Method');
+            elseif strcmp(method, 'approx-face-latice') % over-approximate analysis using face-latice
+                fprintf('\nNNV have not yet support Approximate Face-Latice Method');
+            end
+                        
+        end
+        
+    end
+        
     
 end
 

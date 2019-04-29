bae06201ae2d65f09afddcae2cc1d1c59973024e
trhoangdung <trhoangdung@gmail.com>
2019-04-29 17:17:07 -0500

    update Chelsea-MountainCar dynamics

--- nnv0.1/examples/NNCS/Verisig-MountainCar/Chelsea-MountainCar/discrete_car_dynamics.m
+++ nnv0.1/examples/NNCS/Verisig-MountainCar/Chelsea-MountainCar/discrete_car_dynamics.m
@@ -1,7 +1,7 @@
 function [dx]=discrete_car_dynamics(t,x,u, T)
 T = [];
 % always needs T like this? it is weird to run CORA
-dx(1,1)=x(2)*0.01 + x(1);
-dx(2,1)= (-0.025*cos(3*x(1)) + 0.015 * u)*0.01 + x(2);
+dx(1,1)=x(2)*0.1 + x(1);
+dx(2,1)= (-0.025*cos(3*x(1)) + 0.015 * u)*0.1 + x(2);
 
 

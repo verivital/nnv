00b12a916cdc109650b7ec9474ec7e51a3f3db15
ttj <taylor.johnson@gmail.com>
2019-05-22 11:54:53 -0500

    split path add script into startup.m from install.m, this can be run alone once setup is done with install.m, and is a bit faster than doing the whole install

--- nnv/install.m
+++ nnv/install.m
@@ -11,23 +11,15 @@ cd(tbxmanager_folder);
 urlwrite('http://www.tbxmanager.com/tbxmanager.m', 'tbxmanager.m');
 tbxmanager
 savepath
-tbxmanager restorepath
 fprintf('\nInstalling tbxmanager toolbox is done!');
 
-fprintf('\nIntalling mpt toolbox and other dependencies...');
+fprintf('\nIntalling mpt toolbox and other dependencies...\n');
 tbxmanager install mpt mptdoc;
 tbxmanager install lcp hysdel cddmex clpmex glpkmex fourier sedumi yalmip;
 fprintf('\nInstalling dependencies is done!');
 
-% adding nnv to the path
-fprintf('\nAdding NNV to Matlab path...');
-mydir  = pwd;
-idcs   = strfind(mydir,filesep);
-newdir = mydir(1:idcs(end)-1);
-p = genpath(newdir); % generate a path that includes NNV folder and all folders below it
-addpath(p);
+startup; % adding dependencies and nnv to the path
+
 fprintf('\nInstalling NNV is done, it is ready to use.');
 fprintf('\nPlease go to examples or test folders to run case studies and test examples.');
-fprintf('\nTHANK YOU FOR TRYING NNV. PLEASE EMAIL trhoangdung@gmail.com FOR ANY CONCERNS OR SUGESSTIONS');
-cd(newdir);
-clear;
+fprintf('\nTHANK YOU FOR TRYING NNV. PLEASE EMAIL trhoangdung@gmail.com FOR ANY CONCERNS OR SUGESSTIONS\n\n');

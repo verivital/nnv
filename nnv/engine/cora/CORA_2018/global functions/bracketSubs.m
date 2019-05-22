6d5a80289ac98196da1c34f9eef3061345ffe76a
ttj <taylor.johnson@gmail.com>
2019-05-22 10:01:41 -0500

    remove directly added cora files, going to add as a submodule

--- nnv/engine/cora/CORA_2018/global functions/bracketSubs.m
+++
@@ -1,5 +0,0 @@
-function [str]=bracketSubs(str)
-
-%generate left and right brackets
-str=strrep(str,'L','(');
-str=strrep(str,'R',')');
--- nnv/engine/cora/CORA_2018/models/Cora/vanderPolEq.m
+++
@@ -1,7 +0,0 @@
-function [dx]=vanderPolEq(t,x,u)
-
-mu=1;
-%mu=0.3;
-
-dx(1,1)=x(2);
-dx(2,1)=mu*(1-x(1)^2)*x(2)-x(1)+u(1);

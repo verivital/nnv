09e86507defbfa0cf987154f843d218579e37695
Seth Green <seth@codeocean.com>
2019-05-30 23:22:30 +0000

    Edited install.m, postInstall

--- code/nnv/install.m
+++ code/nnv/install.m
@@ -1,26 +1,26 @@
-% fprintf('\nINSTALLING NNV....')
-% fprintf('\nIntalling tbxmanager (requires Matlab R2009a or later) ...');
-% tbxmanager_folder = 'tbxmanager';
-% list = dir;
-% if ~isfolder(tbxmanager_folder)
-%     mkdir(tbxmanager_folder);
-% end
+fprintf('\nINSTALLING NNV....')
+fprintf('\nIntalling tbxmanager (requires Matlab R2009a or later) ...');
+tbxmanager_folder = 'tbxmanager';
+list = dir;
+if ~isfolder(tbxmanager_folder)
+    mkdir(tbxmanager_folder);
+end
 
-% % install mpt toobox and other dependencies
-% cd(tbxmanager_folder);
-% urlwrite('https://raw.githubusercontent.com/verivital/tbxmanager/master/tbxmanager.m', 'tbxmanager.m');
-% %urlwrite('http://www.tbxmanager.com/tbxmanager.m', 'tbxmanager.m');
-% tbxmanager
-% savepath
-% fprintf('\nInstalling tbxmanager toolbox is done!');
+% install mpt toobox and other dependencies
+cd(tbxmanager_folder);
+urlwrite('https://raw.githubusercontent.com/verivital/tbxmanager/master/tbxmanager.m', 'tbxmanager.m');
+%urlwrite('http://www.tbxmanager.com/tbxmanager.m', 'tbxmanager.m');
+tbxmanager
+savepath
+fprintf('\nInstalling tbxmanager toolbox is done!');
 
-% fprintf('\nIntalling mpt toolbox and other dependencies...\n');
-% tbxmanager install mpt mptdoc;
-% tbxmanager install lcp hysdel cddmex clpmex glpkmex fourier sedumi;
-% tbxmanager install yalmip; % todo: error due to license, need to force acceptance
-% fprintf('\nInstalling dependencies is done!');
+fprintf('\nIntalling mpt toolbox and other dependencies...\n');
+tbxmanager install mpt mptdoc;
+tbxmanager install lcp hysdel cddmex clpmex glpkmex fourier sedumi;
+tbxmanager install yalmip; % todo: error due to license, need to force acceptance
+fprintf('\nInstalling dependencies is done!');
 
-% startup; % adding dependencies and nnv to the path
+startup; % adding dependencies and nnv to the path
 
 fprintf('\nInstalling NNV is done, it is ready to use.');
 fprintf('\nPlease go to examples or test folders to run case studies and test examples.');

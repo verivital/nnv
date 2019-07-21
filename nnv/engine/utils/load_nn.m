19fc55331618c9511f1f6b0215aca0e15b6da90a
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2019-07-20 19:50:37 -0500

    Fixed load_nn function to include mat files

--- nnv/engine/utils/load_nn.m
+++ nnv/engine/utils/load_nn.m
@@ -1,4 +1,4 @@
-function [Controller] = load_nn(PyPath,nnmtPath,input,output,formatin)
+function [Controller] = load_nn(PyPath,nnmtPath,input,output,formatin,opt_mat)
 %% This function loads a neural network and transforms it into a mat file using the nnmt tool
 % % OUTPUTS
 % status = 1 or 0, 1 if we run into an error, 0 if run was succesful
@@ -10,36 +10,68 @@ function [Controller] = load_nn(PyPath,nnmtPath,input,output,formatin)
 % input = path to the neural network we want to transform (includes everything except for the 
 % output = path to the folder we want to store the mat file at
 % formatin = format of neural network to transform
+% opt_mat = input path to nn in mat format (optional)
 %
 % EXAMPLE 
 %load_nn('...\envs\DL1','...\nnmt','...\nnmt\testing\neural_network_information_13','...\nnmt\examples','Sherlock');
 
+% If the is a mat file, no need to parse it through nnmt
+if ~isempty(opt_mat):
+    net_info = load(opt_mat);
+    disp('Neural network loaded');
+else:
+        % Check OS running on
+    osc = computer;
+    if contains(osc,'WIN')
+        sh = '\'; % windows
+    else 
+        sh = '/'; % mac and linux
+    end
 
-% Check OS running on
-osc = computer;
-if contains(osc,'WIN')
-    sh = '\'; % windows
-else 
-    sh = '/'; % mac and linux
+    % Generate the command to run in the terminal
+    commandPy = [PyPath sh 'python ' nnmtPath sh 'nnvmt.py -i ' input ' -o ' output ' -t ' formatin];
+    % Run the command in the terminal and convert the nn
+    [status,cmdout] = system(commandPy);
+
+    % Check if the nn was converted
+    if status == 0
+        disp('Neural Network converted succesfully');
+    else
+        error(cmdout);
+    end
+    name = split(input,'.');
+    name = split(name{1},sh);
+    name = name(end);
+    name = [sh name{1}];
+    net_info = load([output name '.mat']);
+    disp('Neural network loaded');
 end
 
-% Generate the command to run in the terminal
-commandPy = [PyPath sh 'python ' nnmtPath sh 'nnvmt.py -i ' input ' -o ' output ' -t ' formatin];
-% Run the command in the terminal and convert the nn
-[status,cmdout] = system(commandPy);
-
-% Check if the nn was converted
-if status == 0
-    disp('Neural Network converted succesfully');
-else
-    error(cmdout);
-end
-name = split(input,'.');
-name = split(name{1},sh);
-name = name(end);
-name = [sh name{1}];
-net_info = load([output name '.mat']);
-disp('Neural network loaded');
+% % Check OS running on
+% osc = computer;
+% if contains(osc,'WIN')
+%     sh = '\'; % windows
+% else 
+%     sh = '/'; % mac and linux
+% end
+% 
+% % Generate the command to run in the terminal
+% commandPy = [PyPath sh 'python ' nnmtPath sh 'nnvmt.py -i ' input ' -o ' output ' -t ' formatin];
+% % Run the command in the terminal and convert the nn
+% [status,cmdout] = system(commandPy);
+% 
+% % Check if the nn was converted
+% if status == 0
+%     disp('Neural Network converted succesfully');
+% else
+%     error(cmdout);
+% end
+% name = split(input,'.');
+% name = split(name{1},sh);
+% name = name(end);
+% name = [sh name{1}];
+% net_info = load([output name '.mat']);
+% disp('Neural network loaded');
 
 
 % Create the NN object for nnv

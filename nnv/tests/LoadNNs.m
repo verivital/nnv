86377a7b8db1d6a8d996f737e234e6a4a535a69f
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2019-07-24 15:16:13 -0500

    Update nnmt

--- nnv/tests/LoadNNs.m
+++ nnv/tests/LoadNNs.m
@@ -23,13 +23,20 @@ if ~isequal(genp(end-2:end),string(rp)')
 end
 cd ..
 fp = pwd; % Final path used for calling all the future examples
+disp('Coverting neural network 1')
 cont1 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'neural_network_information_13'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Sherlock','');
+disp('Coverting neural network 2')
 cont2 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'neural_network_information_2'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Sherlock','');
+disp('Coverting neural network 3')
 cont3 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'MC_16sigX16sigX1tanh.h5'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Keras','');
+disp('Coverting neural network 4')
 cont4 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'ACASXU_run2a_4_3_batch_2000.nnet'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Reluplex','');
+disp('Coverting neural network 5')
 cont5 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'ACASXU_run2a_5_8_batch_2000.nnet'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Reluplex','');
+disp('Coverting neural network 6')
 cont6 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'CartPole_Controller.h5'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Keras',[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'CartPole_Controller.json']);
-cont7 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'example3tf' sh 'example3.meta'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Tensorflow',[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'example3tf']);
+disp('Coverting neural network 7')
+cont7 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'example2' sh 'example2.meta'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Tensorflow',[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'example2']);
 
 
 

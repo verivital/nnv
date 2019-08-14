29ed36b08c0b8271fd669b8d22f295097d2dcb6f
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2019-08-14 12:31:28 -0500

    Fix error mention in issue #4 in the test_load_nn

--- nnv/tests/io/test_load_nn.m
+++ nnv/tests/io/test_load_nn.m
@@ -17,12 +17,15 @@ pypath = strjoin(e,sh);
 
 % Get paths to the inputs for the first example
 genp = split(string(pwd),sh);
-rp = {'nnv','nnv','tests'};
-if ~isequal(genp(end-2:end),string(rp)')
-    error('Executing from %s. \nPlease, change your current folder path to ../nnv/nnv/tests and run script again',pwd);
+rp = {'nnv','nnv','tests','io'};
+
+if ~isequal(genp(end-3:end),string(rp)')
+    error('Executing from %s. \nPlease, change your current folder path to ../nnv/nnv/io/tests and run script again',pwd);
 end
-cd ..
-fp = pwd; % Final path used for calling all the future examples
+
+fp = genp(1:end-2);
+fp = strjoin(fp,sh);
+fp = char(fp);
 disp('Coverting neural network 1')
 cont1 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'neural_network_information_13'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Sherlock','');
 disp('Coverting neural network 2')
@@ -39,8 +42,3 @@ disp('Coverting neural network 7')
 cont7 = load_nn(pypath,[fp sh 'engine' sh 'nnmt'],[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'example2' sh 'example2.meta'],[fp sh 'engine' sh 'nnmt' sh 'translated_networks'],'Tensorflow',[fp sh 'engine' sh 'nnmt' sh 'original_networks' sh 'example2']);
 
 disp('We have succesfully converted all the neural networks!');
-
-
-%% At the end, delete all variables except for the controllers created
-cd tests
-clearvars -except cont1 cont2 cont3 cont4 cont5 cont6 cont7

a0fb8898bdc846114472afd9b98653d2c9e2effc
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2019-10-03 09:47:11 -0500

    Added some changes to the the loading cnn function and its corresponding tests

--- nnv/tests/io/test_load_cnn.m
+++ nnv/tests/io/test_load_cnn.m
@@ -1,10 +1,14 @@
 %% Run examples to load a CNN from different formats 
+% As of October 2nd, 2019, only cnn1 is complete. Working on adding support
+% to the other layers where the remaining test cases fail.
 % Test 1 - matlab
-cnn1 = load_cnn('matlab','models/TEST_NET.mat');
+cnn1 = load_cnn('matlab','models/TEST_NET.mat'); % Works
 % Test 2 - keras
-cnn2 = load_cnn('keras','models/final_model.h5');
+cnn2 = load_cnn('keras','models/final_model.h5'); % Fails because it has
+% a type of layer that is not supported by nnv yet
 % Test 3 - onnx
-cnn3 = load_cnn('onnx','models/cifarResNet.onnx','classification');
+cnn3 = load_cnn('onnx','models/cifarResNet.onnx','classification'); % Fails 
+% because it has a type of layer that is not supported by nnv yet
 % Test 4 - caffe
 cnn4 = load_cnn('caffe','models/deploy_alexnet.prototxt','models/bvlc_alexnet.caffemodel');
 % Test 5 - onnx

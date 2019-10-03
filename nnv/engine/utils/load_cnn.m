a2455dbd87571ffbfb7f578b2df40a7ea01fc90b
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2019-10-03 09:47:11 -0500

    Added some changes to the the loading cnn function and its corresponding tests

--- nnv/engine/utils/load_cnn.m
+++ nnv/engine/utils/load_cnn.m
@@ -1,5 +1,12 @@
 function cnn = load_cnn(tool,Input,varargin)
 %LOAD_CNN Loads a CNN from multiple formats into Matlab's default structure
+%   For the moment, nnv only supports FullyConnected networks with 1-to-1
+%   connections. This means, that we will only return the Layers in order,
+%   without specifying the connections, as we assume that layer1 is
+%   connected to layer2, layer2 to layer3,..., and layerN-1 to layerN.
+%
+%   The output returns a CNN object used by nnv for its robustness analysis.
+%
 %   Currently we support 4 different formats:
 %       1) Matlab --> tool = matlab, Input = 'name_of_cnn.mat' (Defined as lgraph, DAGNetwork or SeriesNetwork)      
 %       2) ONNX --> tool = onnx, Input = 'name_of_cnn.onnx', varargin = outputType
@@ -44,7 +51,8 @@ if string(class(nn)) == "struct"
         layer1 = imageInputLayer([nn.net.Layers(1,1).InputSize]);
         layer1.AverageImage = zeros(layer1.InputSize);
         temp.Layers(1,1) = layer1;
-        nn.net.loadobj(temp);
+%         nn.net = nn.net.loadobj(temp);
+        nn = SeriesNetwork(temp.Layers);
     end
     cnn = CNN.parse(nn.net,'CNN_c');
 else
@@ -53,7 +61,8 @@ else
         layer1 = imageInputLayer([nn.Layers(1,1).InputSize]);
         layer1.AverageImage = zeros(layer1.InputSize);
         temp.Layers(1,1) = layer1;
-        nn.loadobj(temp);
+%         nn = nn.loadobj(temp);
+        nn = SeriesNetwork(temp.Layers);
     end
     cnn = CNN.parse(nn,'CNN_c');
 end

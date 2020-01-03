39c8c3b7c27007889c1476e707a8b4f3baaebad0
xiaodong <sheldon.uestc@gmail.com>
2020-01-03 14:59:17 -0600

    editted adjust_glpk

--- code/nnv/install.m
+++ code/nnv/install.m
@@ -33,17 +33,23 @@ fprintf('\nPlease go to examples or test folders to run case studies and test ex
 fprintf('\nTHANK YOU FOR TRYING NNV. PLEASE EMAIL trhoangdung@gmail.com FOR ANY CONCERNS OR SUGESSTIONS\n\n');
 
 function adjust_glpk()
-    fid = fopen('tbxmanager/toolboxes/glpkmex/1.0/glnxa64/glpkmex_1_0_glnxa64/glpk.m');
-    cac = textscan( fid, '%s', 'Delimiter','\n','whitespace', '');
-    fclose(fid);
-    fid = fopen('tbxmanager/toolboxes/glpkmex/1.0/glnxa64/glpkmex_1_0_glnxa64/glpk.m', 'w');
-    change_here = 372;
-    for jj = 1 : change_here-1
+    try
+	filename = 'tbxmanager/toolboxes/glpkmex/1.0/glnxa64/glpkmex_1_0_glnxa64/glpk.m';
+        fid = fopen(filename);
+        cac = textscan( fid, '%s', 'Delimiter','\n','whitespace', '');
+        fclose(fid);
+        filename2 = 'tbxmanager/toolboxes/glpkmex/1.0/glnxa64/glpkmex_1_0_glnxa64/glpk.m2';
+        fid = fopen(filename2, 'w');
+        change_here = 372;
+        for jj = 1 : change_here-1
         fprintf(fid, '%s\n', cac{1}{jj});
-    end
-    fprintf(fid, '%s\n', '%clear glpkcc;');
-    for jj = change_here+1: length(cac{1})
+        end
+        fprintf(fid, '%s\n', '%clear glpkcc;');
+        for jj = change_here+1: length(cac{1})
         fprintf(fid, '%s\n', cac{1}{jj});
+        end
+        fclose(fid);
+        movefile(filename2,filename,'f')
+    catch
     end
-    fclose(fid);
 end

9267f4de28444ff66a42fb879108296183cd081f
xiaodong <sheldon.uestc@gmail.com>
2020-01-03 13:06:06 -0600

    move adjust_glpk to install

--- code/nnv/install.m
+++ code/nnv/install.m
@@ -24,10 +24,26 @@ tbxmanager install mpt mptdoc;
 tbxmanager install lcp hysdel cddmex clpmex glpkmex fourier sedumi;
 tbxmanager install yalmip; % todo: error due to license, need to force acceptance
 fprintf('\nInstalling dependencies is done!');
-run engine/adjust_glpk.m
+adjust_glpk;
 
 startup_nnv; % adding dependencies and nnv to the path
 
 fprintf('\nInstalling NNV is done, it is ready to use.');
 fprintf('\nPlease go to examples or test folders to run case studies and test examples.');
 fprintf('\nTHANK YOU FOR TRYING NNV. PLEASE EMAIL trhoangdung@gmail.com FOR ANY CONCERNS OR SUGESSTIONS\n\n');
+
+function adjust_glpk()
+    fid = fopen('../tbxmanager/toolboxes/glpkmex/1.0/glnxa64/glpkmex_1_0_glnxa64/glpk.m');
+    cac = textscan( fid, '%s', 'Delimiter','\n','whitespace', '');
+    fclose(fid)
+    fid = fopen('../tbxmanager/toolboxes/glpkmex/1.0/glnxa64/glpkmex_1_0_glnxa64/glpk.m', 'w');
+    change_here = 372;
+    for jj = 1 : change_here-1
+        fprintf(fid, '%s\n', cac{1}{jj});
+    end
+    fprintf(fid, '%s\n', '%clear glpkcc;');
+    for jj = change_here+1: length(cac{1})
+        fprintf(fid, '%s\n', cac{1}{jj});
+    end
+    fclose(fid);
+end

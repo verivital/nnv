f02919ec874fb50aad0dd1f310764a5a9c939733
trhoangdung <trhoangdung@gmail.com>
2020-01-04 11:32:49 -0600

    add reachability for ReLU layer using ImageZono and its test, work ok

---
+++ code/nnv/tests/nn/cnn/reluLayer/test_ReluLayer_reach_zono.m
@@ -0,0 +1,13 @@
+% construct a FullyConnectedLayer object
+L = ReluLayer();
+LB(:,:,1) = [-0.1 -0.2 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0]; % attack on pixel (1,1) and (1,2)
+LB(:,:,2) = [-0.1 -0.15 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0]; 
+LB(:,:,3) = LB(:,:,2);
+
+UB(:,:,1) = [0 0.2 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0];
+UB(:,:,2) = [0.1 0.15 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0];
+UB(:,:,3) = UB(:,:,2);
+
+image = ImageZono(LB, UB);
+
+image1 = L.reach_zono(image, 'single');

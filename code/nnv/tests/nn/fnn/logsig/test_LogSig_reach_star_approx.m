d572c8ec04a4a736356b0761d9cf1633067e5e87
trhoangdung <trhoangdung@gmail.com>
2020-03-19 15:05:47 -0500

    update reachability method for LogSig class

--- code/nnv/tests/nn/fnn/logsig/test_LogSig_reach_star_approx.m
+++ code/nnv/tests/nn/fnn/logsig/test_LogSig_reach_star_approx.m
@@ -3,7 +3,8 @@ I = ExamplePoly.randVrep;
 V = [0 0; 1 0; 0 1];
 I = Star(V', I.A, I.b); % input star
 
-S = LogSig.reach_star_approx(I);
+S = LogSig.reach_star_approx_no_split(I);
+S1 = LogSig.reach_star_approx_split(I);
 X = I.sample(10);
 Y = LogSig.evaluate(X);
 
@@ -12,9 +13,13 @@ Y = LogSig.evaluate(X);
 figure;
 I.plot; % input set
 hold on;
-S.plot; % reach set
+plot(X(1, :), X(2, :), 'ob'); % sampled inputs
+
+figure;
+Star.plots(S, 'blue'); % reach set
+hold on;
+Star.plots(S1, 'red');
 hold on;
 plot(Y(1, :), Y(2, :), '*'); % sampled outputs
-hold on;
-plot(X(1, :), X(2, :), 'ob'); % sampled inputs
+
 

896116a1fcb436638f77ff29b278a217a41eca35
trhoangdung <trhoangdung@gmail.com>
2020-03-27 16:20:19 -0500

    add main reach method for TanSig class

--- code/nnv/engine/nn/fnn/TanSig.m
+++ code/nnv/engine/nn/fnn/TanSig.m
@@ -43,6 +43,47 @@ classdef TanSig
             
         end
         
+        function S = reach_star_approx_multipleInputs(varargin)
+            % author: Dung Tran
+            % date: 3/27/2020
+            
+            switch nargin
+                case 1
+                    I = varargin{1};
+                    parallel = []; % no parallel computation
+                    method = 'approx-star-no-split';
+                case 2
+                    I = varargin{1};
+                    parallel = varargin{2};
+                    method = 'approx-star-no-split';
+                case 3
+                    I = varargin{1};
+                    parallel = varargin{2};
+                    method = varargin{3};
+                otherwise
+                    error('Invalid number of input arguments');
+            end
+            
+            p = length(I);
+            S = [];
+            if isempty(parallel)
+                
+                for i=1:p
+                    S =[S TanSig.reach_star_approx(I(i), method)];
+                end
+                
+            elseif strcmp(parallel, 'parallel')
+                
+                parfor i=1:p
+                    S =[S, TanSig.reach_star_approx(I(i), method)];
+                end
+                
+            else
+                error('Unknown parallel computation option');
+            end
+
+        end
+        
         % reachability method with star
         function S = reach_star_approx_no_split(I)
             % @I: the input star set
@@ -457,7 +498,289 @@ classdef TanSig
                   
         end
         
+         % dealing with multiple inputs in parallel
+        function S = reach_zono_approx_multipleInputs(varargin)
+            % author: Dung Tran
+            % date: 3/27/2020
+            
+            switch nargin
+                case 1
+                    I = varargin{1};
+                    parallel = []; % no parallel computation
+                case 2
+                    I = varargin{1};
+                    parallel = varargin{2};
+                otherwise
+                    error('Invalid number of input arguments, should be 1 or 2');
+            end
+            
+            p = length(I);
+            S = [];
+            if isempty(parallel)
+                
+                for i=1:p
+                    S =[S TanSig.reach_zono_approx(I(i))];
+                end
+                
+            elseif strcmp(parallel, 'parallel')
+                
+                parfor i=1:p
+                    S =[S, TanSig.reach_zono_approx(I(i))];
+                end
+                
+            else
+                error('Unknown parallel computation option');
+            end
+
+        end
+         
     end
+    
+    
+    methods(Static) % reachability using abstract domain
+        
+        function S = stepTanSig_absdom(I, index, l, u, y_l, y_u, dy_l, dy_u)
+            % @I: input star set
+            % @index: index of the neuron
+            % @l: l = min(x[index]), lower bound at neuron x[index] 
+            % @u: u = min(x[index]), upper bound at neuron x[index]
+            % @y_l: = tansig(l); output of tansig at lower bound
+            % @y_u: = tansig(u); output of tansig at upper bound
+            % @dy_l: derivative of tansig at the lower bound
+            % @dy_u: derivative of tansig at the upper bound
+
+            % @S: output star set
+
+            % author: Dung Tran
+            % date: 3/27/2020
+
+            if l == u
+                new_V = I.V;
+                new_V(index, 1:I.nVar+1) = 0;
+                new_V(index, 1) = y_l;
+                S = Star(new_V, I.C, I.d, I.predicate_lb, I.predicate_ub);                
+            elseif l >= 0
+                % y is convex when x >= 0
+                % constraint 2: y <= y'(u) * (x - u) + y(u) 
+                % constraint 3: y >= (y(u) - y(l)) * (x - l) / (u - l) + y(l);
+
+
+                n = I.nVar + 1;
+                % over-approximation constraints 
+                % constraint 2: y <= y'(u) * (x - u) + y(u)
+                C2 = [-dy_u*I.V(index, 2:n) 1];
+                d2 = dy_u * I.V(index, 1) - dy_u*u + y_u;
+                % constraint 3: y >= (y(u) - y(l)) * (x - l) / (u - l) + y(l);
+                a = (y_u - y_l)/(u - l);
+                C3 = [a*I.V(index, 2:n) -1];
+                d3 = a*l - y_l - a*I.V(index, 1);
+
+                m = size(I.C, 1);
+                C0 = [I.C zeros(m, 1)];
+                d0 = I.d;
+                new_C = [C0; C2; C3];
+                new_d = [d0; d2; d3];
+                new_V = [I.V zeros(I.dim, 1)];
+                new_V(index, :) = zeros(1, n+1);
+                new_V(index, n+1) = 1; 
+
+                % update predicate bound
+                new_predicate_lb = [I.predicate_lb; y_l]; 
+                new_predicate_ub = [I.predicate_ub; y_u];
+                S = Star(new_V, new_C, new_d, new_predicate_lb, new_predicate_ub);
+
+
+            elseif u <= 0
+                % y is concave when x <= 0
+                % constraint 1: y >= y'(l) * (x - l) + y(l)
+                % constraint 3: y <= (y(u) - y(l)) * (x -l) / (u - l) + y(l);
+
+                n = I.nVar + 1;
+                % over-approximation constraints 
+                % constraint 1: y >= y'(l) * (x - l) + y(l)
+                C1 = [dy_l*I.V(index, 2:n) -1];
+                d1 = -dy_l * I.V(index, 1) + dy_l*l - y_l; 
+                % constraint 3: y <= (y(u) - y(l)) * (x - l) / (u - l) + y(l);
+                a = (y_u - y_l)/(u - l);
+                C3 = [-a*I.V(index, 2:n) 1];
+                d3 = -a*l + y_l + a*I.V(index, 1);
+
+                m = size(I.C, 1);
+                C0 = [I.C zeros(m, 1)];
+                d0 = I.d;
+                new_C = [C0; C1; C3];
+                new_d = [d0; d1; d3];
+                new_V = [I.V zeros(I.dim, 1)];
+                new_V(index, :) = zeros(1, n+1);
+                new_V(index, n+1) = 1; 
+
+                % update predicate bound
+                new_predicate_lb = [I.predicate_lb; y_l]; 
+                new_predicate_ub = [I.predicate_ub; y_u];
+                S = Star(new_V, new_C, new_d, new_predicate_lb, new_predicate_ub);
+
+
+            elseif l <0 && u >0
+                % y is concave for x in [l, 0] and convex for x
+                % in [0, u]
+                % split can be done here 
+
+                
+                % over-approximation constraints 
+                % constraint 1: y >= y'(l) * (x - l) + y(l)
+                % constraint 2: y <= y'(u) * (x - u) + y(u)
+
+                n = I.nVar + 1;
+
+                dy_min = min(dy_l, dy_u);
+                % constraint 1: y >= y'_min * (x - l) + y(l)
+                C1 = [dy_min*I.V(index, 2:n) -1];
+                d1 = -dy_min * I.V(index, 1) + dy_min*l - y_l; 
+                % constraint 2: y <= y'_min * (x - u) + y(u)
+                C2 = [-dy_min*I.V(index, 2:n) 1];
+                d2 = dy_min * I.V(index, 1) - dy_min*u + y_u;
+
+                m = size(I.C, 1);
+                C0 = [I.C zeros(m, 1)];
+                d0 = I.d;
+                new_C = [C0; C1; C2];
+                new_d = [d0; d1; d2];
+                new_V = [I.V zeros(I.dim, 1)];
+                new_V(index, :) = zeros(1, n+1);
+                new_V(index, n+1) = 1; 
+
+                % update predicate bound
+                new_predicate_lb = [I.predicate_lb; y_l]; 
+                new_predicate_ub = [I.predicate_ub; y_u];
+                S = Star(new_V, new_C, new_d, new_predicate_lb, new_predicate_ub);
+
+            end
+
+        end
+        
+        
+        
+        function S = reach_absdom_approx(I)
+        % @I: star input set
+        % @Z: Star output set
+
+        % author: Dung Tran
+        % date: 3/27/2020
+
+        % reference: An abstract domain for certifying neural networks. Proceedings of the ACM on Programming Languages,
+        % Gagandeep Singh, POPL, 2019
+
+            if ~isa(I, 'Star')
+                error('Input set is not a Star');
+            end
+
+            [l, u] = I.estimateRanges;  
+
+            y_l = tansig(l);
+            y_u = tansig(u);
+            dy_l = tansig('dn', l);
+            dy_u = tansig('dn', u);
+
+            n = I.dim;
+            S = I;
+            for i=1:n
+                S = TanSig.stepTanSig_absdom(S, i, l(i), u(i), y_l(i), y_u(i), dy_l(i), dy_u(i)); 
+            end
+
+            end
+
+        % dealing with multiple inputs in parallel
+        function S = reach_absdom_approx_multipleInputs(varargin)
+            % author: Dung Tran
+            % date: 3/27/2020
+
+            switch nargin
+                case 1
+                    I = varargin{1};
+                    parallel = []; % no parallel computation
+                case 2
+                    I = varargin{1};
+                    parallel = varargin{2};
+                otherwise
+                    error('Invalid number of input arguments, should be 1 or 2');
+            end
+
+            p = length(I);
+            S = [];
+            if isempty(parallel)
+
+                for i=1:p
+                    S =[S TanSig.reach_absdom_approx(I(i))];
+                end
+
+            elseif strcmp(parallel, 'parallel')
+
+                parfor i=1:p
+                    S =[S, TanSig.reach_absdom_approx(I(i))];
+                end
+
+            else
+                error('Unknown parallel computation option');
+            end
+
+        end
+
+    
+    end
+    
+
+methods(Static) % main reach method
+
+    % main function for reachability analysis
+    function R = reach(varargin)
+        % @I: an array of star input sets
+        % @method: 'approx-star' or 'approx-zono' or 'abs-dom' 
+        % @option: = 'parallel' or [] using parallel computation or not
+
+        % author: Dung Tran
+        % date: 3/27/2019
+
+        switch nargin
+
+            case 3
+                I = varargin{1};
+                method = varargin{2};
+                option = varargin{3};
+
+            case 2
+                I = varargin{1};
+                method = varargin{2};
+                option = [];
+            case 1
+                I = varargin{1};
+                method = 'approx-star';
+                option = [];
+            otherwise
+                error('Invalid number of input arguments (should be 1, 2 or 3)');
+        end
+
+
+        if strcmp(method, 'approx-star') % exact analysis using star
+
+            R = TanSig.reach_star_approx_multipleInputs(I, option);
+
+        elseif strcmp(method, 'approx-zono')  % over-approximate analysis using zonotope
+
+            R = TanSig.reach_zono_approx_multipleInputs(I, option);
+
+        elseif strcmp(method, 'abs-dom')  % over-approximate analysis using abstract-domain
+
+            R = TanSig.reach_absdom_approx_multipleInputs(I, option);
+
+        else
+            error('Unknown or unsupported reachability method for layer with LogSig activation function');
+        end
+
+    end
+
+end
+
+
     
     
 end

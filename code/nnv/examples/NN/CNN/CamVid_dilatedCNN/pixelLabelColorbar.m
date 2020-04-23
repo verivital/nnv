f8cd5a912c179577e5c736b69c740d1990464c46
Neelanjana314 <neelanjana.pal@vanderbilt.edu>
2020-04-23 01:23:07 -0500

    Camvid_dilatedCNN Network creation

---
+++ code/nnv/examples/NN/CNN/CamVid_dilatedCNN/pixelLabelColorbar.m
@@ -0,0 +1,19 @@
+function pixelLabelColorbar(cmap, classNames)
+% Add a colorbar to the current axis. The colorbar is formatted
+% to display the class names with the color.
+
+colormap(gca,cmap)
+
+% Add colorbar to current figure.
+c = colorbar('peer', gca);
+
+% Use class names for tick marks.
+c.TickLabels = classNames;
+numClasses = size(cmap,1);
+
+% Center tick labels.
+c.Ticks = 1/(numClasses*2):1/numClasses:1;
+
+% Remove tick mark.
+c.TickLength = 0;
+end

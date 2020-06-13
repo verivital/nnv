b3571fa40de65246b1e1ac8f10e041fce1bbf9ed
trhoangdung <trhoangdung@gmail.com>
2020-06-13 09:29:00 -0500

    update LogSig and PosLin reachability

--- code/nnv/tests/nn/funcs/poslin/test_PosLin_reach_star_approx.m
+++ code/nnv/tests/nn/funcs/poslin/test_PosLin_reach_star_approx.m
@@ -1,7 +1,8 @@
 
-I = ExamplePoly.randVrep;   
-V = [0 0; 1 0; 0 1];
-I = Star(V', I.A, I.b); % input star
+I0 = ExamplePoly.randVrep; 
+I0.outerApprox;
+V = [0 0; 1 1;1 0];
+I = Star(V', I0.A, I0.b, I0.Internal.lb, I0.Internal.ub); % input star
 X = I.sample(100);
 
 figure;
@@ -9,8 +10,13 @@ I.plot;
 hold on;
 plot(X(1, :), X(2, :), 'ob'); % sampled inputs
 
+t = tic;
 S = PosLin.reach_star_approx(I); % over-approximate reach set
+t1 = toc(t);
 S1 = PosLin.reach(I); % exach reach set
+t = tic;
+S2 = PosLin.reach_star_approx2(I); % new-over-approximate method
+t2 = toc(t);
 
 Y = PosLin.evaluate(X);
 
@@ -20,3 +26,6 @@ hold on;
 Star.plots(S1);
 hold on;
 plot(Y(1, :), Y(2, :), '*'); % sampled outputs
+
+figure;
+Star.plots(S2);

93992d19dc96d8bbf8fb996cffd97b26db1d7451
Neelanjana Pal <38669616+Neelanjana314@users.noreply.github.com>
2021-06-27 12:15:57 -0500

    update comments

--- code/nnv/examples/Submission/VNN_COMP2021/run_reachability.m
+++ code/nnv/examples/Submission/VNN_COMP2021/run_reachability.m
@@ -8,7 +8,7 @@ function [status, total_time] = run_reachability(onnxfile,vnnlibfile)%)%ip_shape
     %variables: imagestar_set,op_specs_mat, op_specs_vec,ip_bounds 
     [~,vnnfile,~] = fileparts(vnnlibfile);
     vnnfile = vnnfile + ".mat";
-    load(vnnfile)
+    load(vnnfile);
     
     %variables: net for nnv format 
     [~,netfilename,~] = fileparts(onnxfile);
@@ -19,9 +19,9 @@ function [status, total_time] = run_reachability(onnxfile,vnnlibfile)%)%ip_shape
     [op_bounds, total_time] = net.reach(imagestar_set);
 
     if isempty(op_bounds.toStar.intersectHalfSpace(op_specs_mat, op_specs_vec))%(:,j)(i,j) for exact star
-        status = 1; % Safe if no intersection with the op_spec/unsafe region
+        status = 1; % Safe if no intersection with the op_spec/unsafe region--- violates the counter-ex spec
     else
-        status = 0; %Unknown if intersection with the op_spec/unsafe region; for approx-star)
+        status = 0; %Unknown if intersection with the op_spec/unsafe region; for approx-star-- holds/unknown for the counter-ex spec
         %break
     end
 end

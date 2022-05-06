5e3f23ef41ae2691c51996168ff386133664a75b
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2022-05-06 15:24:17 -0400

    Cleaning up more code

--- code/nnv/examples/Submission/FORMATS2022/Comparison/nnv_reach/CTRNN_DampedForcedPendulum_reach.m
+++ code/nnv/examples/Submission/FORMATS2022/Comparison/nnv_reach/CTRNN_DampedForcedPendulum_reach.m
@@ -29,17 +29,4 @@ R = model.stepReachStar(init_set,input_set);
 time = toc(t);
 Rall = model.intermediate_reachSet;
 
-save('../results/dfp_reach.mat','Rall','time')
-
-% Plot reachable sets
-f = figure;
-Star.plotBoxes_2D_noFill(Rall,1,2,'b');
-grid;
-hold on;
-% for sets = model.intermediate_reachSet
-%     Zono.plots(sets.Z); % This is super slow, try saving using CORA
-%     zonotopes and use their plotting routines (Tho this plots the actual reach set, not an overapproximation)
-% end
-xlabel('x1');
-ylabel('x2');
-saveas(f,'../results/CTRNN_DFP_nnv.png');
+save('../results/fpa_reach.mat','Rall','time')

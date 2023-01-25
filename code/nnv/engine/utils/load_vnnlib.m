1945740e097e886a3ec164d5b4a62d56af4f3657
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2023-01-25 17:36:32 -0600

    Update vnnlib to support multiple constraints within OR statement

--- code/nnv/engine/utils/load_vnnlib.m
+++ code/nnv/engine/utils/load_vnnlib.m
@@ -1,10 +1,25 @@
-function spec = load_vnnlib(propertyFile)
+function property = load_vnnlib(propertyFile)
+    % property = load_vnnlib(propertyFile)
+    % input: vnnlib file describing property to verify
+    % output:
+    %     - property (struct) with following fields
+    %         - lb    -> input lower bound vector
+    %         - ub    -> input upper bound vector
+    %         - prop  -> collection of HalfSpaces describing output specification to verify
+
+%   Assumptions for this function
+%     1) Unique input set, only one value for the upper and lower bounds of the inputs
+%     2) Output property
+%         a) Series of assertions with no "and", "or" conditions                ---->  property.prop: (1x1) HalfSpace
+%         b) One assertion with one or more "and", no "or" included             ---->  property.prop: (1x1) HalfSpace
+%         c) One assertion with 1 "or" statement followed by N "and" statements ---->  property.prop: (Nx1) HalfSpace
+%     3) Constraints must be linear, only >= or <= are supported
+%
+%    If the vnnlib does not meet these conditions, this function may not work
 
     fileID = fopen(propertyFile,'r'); % open vnnlib file
     tline = fgetl(fileID); % Reading file line-by-line
     phase = "start"; % there are four phases in each file (declare inputs, declare outputs, define inputs, define outputs)
-    spec = struct; % Specification, returned as a struct, could also be a list of structs (fields: lb, ub, halfspace)
-    prop_count = 1;
     
     while ischar(tline)
         if isempty(tline) || startsWith(tline, ';')    % line contains no information
@@ -32,8 +47,6 @@ function spec = load_vnnlib(propertyFile)
                 dim = dim + 1; % only have seen inputs defined as vectors, so this should work
                 % the more general approach would require some extra work, but should be easy as well
             elseif contains(tline, "assert")
-%                 lb_output = zeros(dim,1);
-%                 ub_output = zeros(dim,1);
                 output_dim = dim;
                 dim = 1; % reset dimension counter
                 phase = "DefineInput";
@@ -80,10 +93,22 @@ function spec = load_vnnlib(propertyFile)
                 end
                 % add assertion (ast) to the property
                 if isempty(property.prop)
-                    property.prop{prop_count} = ast;
-                    prop_count = prop_count + 1;
+                    property.prop = {ast};
                 else
-%                     if 
+                    [~, n2] = size(property.prop); % one or multiple halfspace
+                    last_ast = property.prop{n2};
+                    if length(last_ast) > 1 % means previous ast was an "or"
+                        property.prop{n2+1} = ast;
+                    else
+                        if ast.dim == last_ast.dim
+                            last_ast.Hg.G = [last_ast.Hg.G; ast.Hg.G];
+                            last_ast.Hg.g = [last_ast.Hg.g; ast.Hg.g];
+                            property.prop{n2} = last_ast;
+                        else
+                            property.prop{n2+1} = ast;
+                        end
+                    end
+
                 end
             end
         else
@@ -97,7 +122,6 @@ function spec = load_vnnlib(propertyFile)
         % Go to next line
         tline = fgetl(fileID);
     end % end while loop
-%     spec = property;
     fclose(fileID); % close vnnlib file
 end % end function
 
@@ -115,13 +139,18 @@ function tline = merge_lines(tline, fileID)
     end
 end
 
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%%%  ASSERTION   %%%%
+%%%%%%%%%%%%%%%%%%%%%%
 function ast = process_assertion(tline, dim)
     ast = struct;           % initialize assertion
     ast.dim = dim;          % output dimension
     ast.Hg = {};            % collection of HalfSpaces
     tline = strtrim(tline); % remove leading and trailing whitespaces
     % start processing assertion
-    tline = tline(7:end); % remove "(assert" from tline
+    tline = tline(8:end); % remove "(assert" from tline
     while tline
         if length(tline) == 1 && tline == ')'
             break
@@ -133,7 +162,7 @@ function ast = process_assertion(tline, dim)
             % if a single constraint, H would be a [1,dim] mat, and g a scalar
             % These get added to ast.Hg
             [ast, len] = process_constraint(tline, ast);
-            tline = tline(len:end); % update tline, process remainder of assertion
+            tline = tline(len+1:end); % update tline, process remainder of assertion
             if isempty(ast.Hg)
                 ast.Hg = HalfSpace(ast.H, ast.g);
             else
@@ -142,11 +171,8 @@ function ast = process_assertion(tline, dim)
                     % only one halfspace yet, we can concatenate the assertion here (similar to and statement)
                     ast.Hg.G = [ast.Hg.H; ast.H ];
                     ast.Hg.g = [ast.Hg.g; ast.g];
-                elseif n2 > n1
-                    % we have an array of Halfspace (which means an "or" property predecesess this property, create new HalfSpace
-                    ast.Hg{n2+1} = HalfSpace(ast.H, ast.g);
                 else
-                    error("We should not be here, go back and fix this error!! No other option to add H and g to property");
+                    error("Error or property not supported after processing a constraint.");
                 end
 
             end % close
@@ -155,13 +181,12 @@ function ast = process_assertion(tline, dim)
             % if multiple *or* statements, H -> cell array of [1,dim], g -> cell array of scalar
             tline = tline(4:end);
 %             pars = 1; % keep track of parenthesis opened during "or" statement
-            [ast, tline] = process_or(tline, ast);
-
+            [ast, tline] = process_or(tline, ast); % there is only one allowed, nothing can come after closing the or statement
+            
         elseif startsWith(tline,'(and')
             % parse all constraints with the "and" statement
             % if multiple *and* statements, then H -> [# ands, dim], and g a [# ands, 1]
-            tline = tline(5:end);
-            [ast, tline] = process_and(tline, ast);
+            error('Property not supported for now. Not allowed -> assertion starting with AND and no OR.')
 
         elseif startsWith(tline, ')')
             tline = tline(2:end);
@@ -171,9 +196,13 @@ function ast = process_assertion(tline, dim)
     end
 end
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%   CONSTRAINT  %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%
 function [ast, len] = process_constraint(tline, ast)
     % process a single constraint
-    len = 0;
+    len = 1;
     while tline(len) ~= ')'
         len  = len + 1;
     end
@@ -183,14 +212,16 @@ function [ast, len] = process_constraint(tline, ast)
     var1 = const{2};
     idx1 = split(var1,'_');
     idx1 = str2double(idx1{2})+1;
-    var2 = const{3};    
+    var2 = const{3};  
     % initialize values
-    H = zeros(1, dim);
+    H = zeros(1, ast.dim);
     g = 0; 
     if contains(op,"<=") % no need to change signs, same as in halfspace
         H(idx1) = 1;
         if contains(var2,'Y')
-            idx2 = split(var1,'_');
+            var2 = split(var2, ')');
+            var2 = var2{1};
+            idx2 = split(var2,'_');
             idx2 = str2double(idx2{2})+1;
             H(idx2) = -1;
         else
@@ -200,7 +231,9 @@ function [ast, len] = process_constraint(tline, ast)
     else
         H(idx1) = -1;
         if contains(var2,'Y')
-            idx2 = split(var1,'_');
+            var2 = split(var2, ')');
+            var2 = var2{1};
+            idx2 = split(var2,'_');
             idx2 = str2double(idx2{2})+1;
             H(idx2) = 1;
         else
@@ -213,71 +246,86 @@ function [ast, len] = process_constraint(tline, ast)
     ast.g = g;
 end
 
-function [ast, tline] = process_and(tline, ast)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%    AND    %%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%
+function [temp_ast, tline] = process_and(tline, ast)
     temp_ast.dim = ast.dim;
     temp_ast.Hg = {};
-    while tline
+    params = 1; % from the removed '(and'
+    while ~isempty(tline) && params > 0
         tline = strtrim(tline);
         if startsWith(tline,'(<=') || startsWith(tline,'(>=')
-            % get the mat and vector of one constraint
-            % if a single constraint, H would be a [1,dim] mat, and g a scalar
-            % These get added to ast.Hg
-            [ast, len] = process_constraint(tline, temp_ast);
-            tline = tline(len:end); % update tline, process remainder of assertion
+            [temp_ast, len] = process_constraint(tline, temp_ast);
+            tline = tline(len+1:end); % update tline, process remainder of assertion
+            if isempty(temp_ast.Hg)
+                temp_ast.Hg = HalfSpace(temp_ast.H, temp_ast.g);
+            else
+                temp_ast.Hg.G = [temp_ast.Hg.G; temp_ast.H];
+                temp_ast.Hg.g = [temp_ast.Hg.g; temp_ast.g];
+            end
+            
         elseif startsWith(tline,'(or')
-            % parse all constraints with the "or" statement
-            % if multiple *or* statements, H -> cell array of [1,dim], g -> cell array of scalar
-            tline = tline(4:end);
-    %             pars = 1; % keep track of parenthesis opened during "or" statement
-            [ast, tline] = process_or(tline, temp_ast);
-    
+            error("Currently we do not support an OR statement within and AND statement. ")
+    %             
         elseif startsWith(tline,'(and')
-            % parse all constraints with the "and" statement
-            % if multiple *and* statements, then H -> [# ands, dim], and g a [# ands, 1]
-            tline = tline(5:end);
-            [ast, tline] = process_and(tline, temp_ast);
+            error("Currently we do not support an AND statement within and AND statement. ")
 
         elseif startsWith(tline, ')')
-            break
+            params = params - 1;
         else
-            error("We may be doing something wrong while processing the or statement.")
+            error("We may be doing something wrong while processing the AND statement or the property is currently not supported.")
         end
     end
-    % add temp_ast to ast
-    % TODO
 end
 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%    OR    %%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%
 function [ast, tline] = process_or(tline, ast)
     temp_ast.dim = ast.dim;
     temp_ast.Hg = {};
-    while tline
+    pars = 1;
+    while ~isempty(tline) > 0 && pars > 0
         tline = strtrim(tline);
         if startsWith(tline,'(<=') || startsWith(tline,'(>=')
             % get the mat and vector of one constraint
             % if a single constraint, H would be a [1,dim] mat, and g a scalar
             % These get added to ast.Hg
-            [ast, len] = process_constraint(tline, temp_ast);
-            tline = tline(len:end); % update tline, process remainder of assertion
+            [or_ast, len] = process_constraint(tline, temp_ast);
+            tline = tline(len+1:end); % update tline, process remainder of assertion
+            or_ast.Hg = HalfSpace(or_ast.H, or_ast.g);
+            if isempty(temp_ast.Hg)
+                temp_ast = or_ast; % first HalfSpace within OR statement
+            else
+                temp_ast.Hg = [temp_ast.Hg; or_ast.Hg]; % add HalfSpace to current assertion
+            end
         elseif startsWith(tline,'(or')
-            % parse all constraints with the "or" statement
-            % if multiple *or* statements, H -> cell array of [1,dim], g -> cell array of scalar
-            tline = tline(4:end);
-    %             pars = 1; % keep track of parenthesis opened during "or" statement
-            [ast, tline] = process_or(tline, temp_ast);
+            error("Currently we do not support an OR statement within and OR statement.")
     
         elseif startsWith(tline,'(and')
             % parse all constraints with the "and" statement
             % if multiple *and* statements, then H -> [# ands, dim], and g a [# ands, 1]
             tline = tline(5:end);
-            [ast, tline] = process_and(tline, temp_ast);
-            
+            pars = pars + 1;
+            [or_ast, tline] = process_and(tline, temp_ast);
+            if isempty(temp_ast.Hg)
+                temp_ast = or_ast; % first HalfSpace within OR statement
+            else
+                temp_ast.Hg = [temp_ast.Hg; or_ast.Hg]; % add HalfSpace to current assertion
+            end
         elseif startsWith(tline, ')')
-            break
+            pars = pars - 1;
+            tline = tline(2:end);
         else
-            error("We may be doing something wrong while processing the or statement.")
+            error("Something went wrong while processing the OR statement.")
         end
+
     end
-    % add temp_ast to ast
-    % TODO
+    ast = temp_ast; % only allow 1 OR statement, so it should be the one and only assertion in the vnnlib file
 end
 

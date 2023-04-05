ef344c01d398ab43a001995fb4197e4cef6ae1b3
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2023-04-05 17:06:32 -0500

    Speed up Box constructor

--- code/nnv/engine/set/Box.m
+++ code/nnv/engine/set/Box.m
@@ -34,18 +34,21 @@ classdef Box
             
             obj.center = 0.5 * (lb + ub);
             vec = 0.5 * (ub - lb);
-            for i=1:n1
-                if vec(i) ~= 0
-                    gen = cast(zeros(n1, 1), 'like', ub);
-                    gen(i) = vec(i);
-                    obj.generators = [obj.generators gen];
-                end                
-            end
-            
-            if norm(vec) == 0
-                obj.generators = cast(zeros(obj.dim, 1), 'like', ub);
-            end
-                
+%             for i=1:n1
+%                 if vec(i) ~= 0
+%                     gen = cast(zeros(n1, 1), 'like', ub);
+%                     gen(i) = vec(i);
+%                     obj.generators = [obj.generators gen];
+%                 end                
+%             end
+%             
+%             if norm(vec) == 0
+%                 obj.generators = cast(zeros(obj.dim, 1), 'like', ub);
+%             end
+            % Speeding up implementation
+            gens = diag(vec); % generate matrix
+            gens(:,all(gens(gens==0))) = []; % delete colums with no info
+            obj.generators = gens;
         end
         
         % single partition of a box

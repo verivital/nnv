016b5da8f207f5cb047656a03353d5c068abbe60
mldiego <diego.manzanas.lopez@vanderbilt.edu>
2023-06-29 17:30:19 -0500

    installation script vnncomp2023

---
+++ code/nnv/examples/Submission/VNN_COMP2023/install_tool.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Installation script used for VNN-COMP. Ubuntu, MATLAB R2022b (https://github.com/verivital/nnv)
+
+TOOL_NAME="nnv"
+VERSION_STRING="v1"
+
+# check arguments
+if [ "$1" != ${VERSION_STRING} ]; then
+	echo "Expected first argument (version string) '$VERSION_STRING', got '$1'"
+	exit 1
+fi
+
+echo "Installing $TOOL_NAME dependencies"
+
+sudo apt install -y python3-pip
+pip install numpy onnxruntime onnx scipy matlabengine
+
+# remove paths from any prior installation
+matlab -nodisplay -r "rmpath(genpath('/home/ubuntu/work/nnv/')); savepath; quit"
+
+matlab -nodisplay -r "cd '/home/ubuntu/work/nnv/code/nnv/'; install; addpath(genpath('/home/ubuntu/work/nnv/')); savepath; quit"
+
+matlab -nodisplay -r "matlabshared.supportpkg.getInstalled; matlabshared.supportpkg.getSupportPackageRoot; matlabroot; quit"

a85a759f6385543ecebf1b8773c524a93d5c1ee0
mldiego <diego.manzanas.lopez@vanderbilt.edu>
2023-06-29 22:02:02 -0500

    still checking if matlab is installed

--- code/nnv/examples/Submission/VNN_COMP2023/install_tool.sh
+++ code/nnv/examples/Submission/VNN_COMP2023/install_tool.sh
@@ -13,16 +13,23 @@ fi
 
 echo "Installing $TOOL_NAME dependencies"
 
-# sudo apt install -y python3-pip
+sudo apt install -y python3-pip net-tools
 # pip install numpy matlabengine
+echo "Finding MATLAB (directory)"
 find / -type d -name "MATLAB" # is matlab installed? where?
+echo "Finding MATLAB (anything)"
+find "MATLAB" # is matlab installed? where?
+echo "Finding matlab (executable ?)"
+find matlab
+echo "... Finding R2022b"
+find R2022b
 
 ifconfig
 
 # remove paths from any prior installation
-matlab -nodisplay -r "rmpath(genpath('/home/ubuntu/work/nnv/')); savepath; quit"
+#matlab -nodisplay -r "rmpath(genpath('/home/ubuntu/work/nnv/')); savepath; quit"
 
-matlab -nodisplay -r "cd '/home/ubuntu/work/nnv/code/nnv/'; install; addpath(genpath('/home/ubuntu/work/nnv/')); savepath; quit"
+#matlab -nodisplay -r "cd '/home/ubuntu/work/nnv/code/nnv/'; install; addpath(genpath('/home/ubuntu/work/nnv/')); savepath; quit"
 
 # get paths to ensure support packages are set properly
-matlab -nodisplay -r "matlabshared.supportpkg.getInstalled; matlabshared.supportpkg.getSupportPackageRoot; matlabroot; ver; quit"
+#matlab -nodisplay -r "matlabshared.supportpkg.getInstalled; matlabshared.supportpkg.getSupportPackageRoot; matlabroot; ver; quit"

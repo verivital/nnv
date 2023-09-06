d7212ece5b5b139955f62f40d2b4b1439efbabd8
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2023-09-06 05:13:38 -0500

    Clean up most NNCS examples

--- code/nnv/examples/NNCS/InvertedPendulum/reach_invP.m
+++ code/nnv/examples/NNCS/InvertedPendulum/reach_invP.m
@@ -1,16 +1,24 @@
+% Reachability analyisis of inverted pendulum NNCS
+
+%% NNCS
 load controller.mat;
 load sysd.mat;
 
+% Controller
 L1 = LayerS(W{1,1}, b{1,1}, 'poslin');
 L2 = LayerS(W{1,2}, b{2,1}, 'purelin');
+Controller = NN({L1;L2});
+Controller.InputSize = 4;
+Controller.OutputSize = 1;
 
-% NN_Controller = FFNNS([L1 L2]); % feedforward neural network controller
-NN_Controller = NN({L1;L2});
+% Plant
 Plant = DLinearODE(A, B, C, D, Ts);
+
+% NNCS
 feedbackMap = [0]; % feedback map, y[k] 
+ncs = NNCS(Controller, Plant, feedbackMap); % the neural network control system
 
-ncs = NNCS(NN_Controller, Plant, feedbackMap); % the neural network control system
-
+%% Reachability analysis
 % initial set of state of the plant x = [-0.1 <= x[1] <= -0.05, 0.85 <= x[2] <= 0.9, x[3] = 0; x[4] = 0]
 lb = [0.08; 0.2; 0; 0];
 ub = [0.1; 0.4; 0; 0];
@@ -32,8 +40,9 @@ maps = [1 0 0 0; 0 1 0 0];
 
 Pos_Vel_ReachSet = [];
 for i=1:length(P1)
-    Pos_Vel_ReachSet = [Pos_Vel_ReachSet P1.affineMap(maps)];
+    Pos_Vel_ReachSet = [Pos_Vel_ReachSet P1(i).affineMap(maps,[])];
 end
 
+% Visualize
 figure;
-Pos_Vel_ReachSet.plot;
+Star.plots(Pos_Vel_ReachSet);

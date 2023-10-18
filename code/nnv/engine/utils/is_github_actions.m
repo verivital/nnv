6383270fd2dec40928ab97fa534dceb1b3128e28
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2023-10-18 11:31:43 -0500

    Finalizing CI

--- code/nnv/engine/utils/is_github_actions.m
+++ code/nnv/engine/utils/is_github_actions.m
@@ -1,12 +1,6 @@
 function out = is_github_actions()
     nnvpath = nnvroot();
     file_path = [nnvpath, '/github_actions.txt'];
-    % for debugging
-    disp(nnvpath);
-    disp(file_path);
-    allfiles = dir(nnvpath);
-    tableFiles = struct2table(allfiles);
-    disp(tableFiles);
     if isfile(file_path)
         out = 1;
         % 'github actions detected'
--- code/nnv/tests/tutorial/test_all_tutorial.m
+++ code/nnv/tests/tutorial/test_all_tutorial.m
@@ -7,18 +7,10 @@ if ~is_github_actions() % importers (support packages) are not installed
     verify_onnx_vnnlib;
 end
 
-%% 2) NN: GTSRB (verify_falsify_1)
-cd ../GTSRB;
-verify_falsify_1;
 
 %% 3) NN: GTSRB (verify_robust_13)
+cd ../GTSRB;
 verify_robust_13;
-
-%% 4) NN: GTSRB (verify_robust_27)
-verify_robust_27;
-
-%% 5) NN: GTSRB (verify_unknown_1)
-verify_unknown_1;
 
 %% 6) NN: MNIST (input set examples)
 cd ../MNIST;
@@ -41,9 +33,6 @@ verify;
 %% 11) NNCS: AEBS (reach)
 cd ../../AEBS;
 reach;
-
-%% 12) NNCS: AEBS (reach_full_brake)
-reach_full_brake;
 
 %% 13) NNCS: Inverted Pendulum
 cd ../InvertedPendulum;

561a3902adfdfd6080909226a0b263d63d157461
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2023-11-03 16:45:56 -0500

    Other single pendulum examples

---
+++ code/nnv/examples/NNCS/Single_Pendulum/reach_single.m
@@ -0,0 +1,57 @@
+%% Reachability analysis of Single Pendulum Benchmark
+    
+%% Load Components 
+
+% Load the controller
+net = load_NN_from_mat('controller_single_pendulum.mat');
+% Load plant
+reachStep = 0.01;
+controlPeriod = 0.05;
+plant = NonLinearODE(2,1,@dynamics_sp, reachStep, controlPeriod, eye(2));
+% plant.set_tensorOrder(2);
+
+
+%% Reachability analysis
+
+% Initial set
+lb =  [0; -0.1];
+ub = [1; 0.1];
+init_set = Star(lb,ub);
+% Store all reachable sets
+num_steps = 20;
+reachOptions.reachMethod = 'approx-star';
+
+reachAll = init_set;
+t = tic;
+% Begin NNCS reachability for initial set 
+for i = 1:num_steps
+    % Compute controller output set
+    input_set = net.reach(init_set,reachOptions);
+    input_set = Star.get_hypercube_hull(input_set);
+    input_set = input_set.toStar;
+    % Compute plant reachable set
+    init_set = plant.stepReachStar(init_set, input_set,'lin');
+    init_set = Star.get_hypercube_hull(init_set);
+    init_set = init_set.toStar;
+    reachAll = [reachAll init_set];
+end
+toc(t);
+
+%% Visualize results
+timeVector = 0:controlPeriod:controlPeriod*num_steps;
+
+f = figure;
+hold on;
+rectangle('Position',[0.5,1,1,1],'FaceColor',[1 0 0 0.5],'EdgeColor','r', 'LineWidth',0.1)
+Star.plotRanges_2D(reachAll,1,timeVector,'b');
+grid;
+xlabel('Time (s)');
+ylabel('\theta');
+% xlim([0 0.6])
+% ylim([0.95 1.25])
+% Save figure
+if is_codeocean
+    exportgraphics(f,'/results/logs/singlePendulum.pdf', 'ContentType', 'vector');
+else
+    exportgraphics(f,'singlePendulum_single.pdf','ContentType', 'vector');
+end

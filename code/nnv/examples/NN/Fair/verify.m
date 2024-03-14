feb7882c858e2677ca466e8cfdbbbbd1f0229b03
Anne Tumlin <amtumlin@gmail.com>
2024-03-14 13:43:47 -0500

    Beginning to work on the verification steps.

--- code/nnv/examples/NN/Fair/verify.m
+++ code/nnv/examples/NN/Fair/verify.m
@@ -1,1 +1,23 @@
-%% Fairness Verification of Adult Model (NN)
+%% Fairness Verification of Adult Classification Model (NN)
+
+% Load network 
+ac_model = load('adult_model_fc.mat');
+
+% Create NNV model 
+net = matlab2nnv(ac_model.net);
+
+%% Verification
+% Utilize the verify_robustness function?
+% Need to initalize IS (input set), reachOptions, and target
+
+% First, we define the reachability options
+reachOptions = struct; % initialize
+reachOptions.reachMethod = 'approx-star'; % using approxiate method
+
+% Second, we define the input set
+
+
+%Third, we define the target
+
+
+

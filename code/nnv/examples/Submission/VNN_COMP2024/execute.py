7491c306497bc62cae40cf52f44e81a529549487
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2024-07-06 00:06:04 -0500

    Add path to installed support packages from mpm

--- code/nnv/examples/Submission/VNN_COMP2024/execute.py
+++ code/nnv/examples/Submission/VNN_COMP2024/execute.py
@@ -52,6 +52,7 @@ def run_instance(category, onnx, vnnlib, timeout, outputlocation) -> None:
 
     eng.addpath(os.getcwd())
     eng.addpath(eng.genpath('/home/ubuntu/toolkit/code/nnv/'))
+    eng.addpath(eng.genpath('/root/Documents/MATLAB/SupportPackages/R2024a')) # This is where the support packages get installed from mpm
 
     status = 2 #initialize with an 'Unknown' status
     #toc = time.perf_counter()

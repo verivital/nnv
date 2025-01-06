5a03077bf0619b270b1a63131b83141fb5be17cb
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2025-01-05 23:26:20 -0600

    Working on tutorial examples

--- code/nnv/examples/Tutorial/SPIE/Classification2D/verifyDataset.m
+++ code/nnv/examples/Tutorial/SPIE/Classification2D/verifyDataset.m
@@ -1,4 +1,4 @@
-function results = verify_medmnist2d(net, inputs, targets, attack, max_value, min_value)
+function results = verifyDataset(net, inputs, targets, attack, max_value, min_value)
     % verify medmnist with inputs (input images), targets (labels) and attack
     % (struct with adversarial attack info)
     % results =  verify_medmnist(inputs, targets, attack, max_value*, min_value*)
@@ -31,7 +31,7 @@ function results = verify_medmnist2d(net, inputs, targets, attack, max_value, mi
     reachOptions = struct;
     reachOptions.reachMethod = 'approx-star';
     
-    % Evaluate all images
+    % Analyze all images
     for i = 1:N
 
         % print progress
@@ -41,45 +41,16 @@ function results = verify_medmnist2d(net, inputs, targets, attack, max_value, mi
 
         % Create set of images
         img = inputs(:,:,:,i);
-        I = l_inf_attack(img, epsilon, max_value, min_value);
+        I = l_inf_set(img, epsilon, max_value, min_value);
+        target = targets(i);
 
         t = tic; % start timer
-
-        % Check for missclassification
-        img = single(img);
-        y = net.evaluate(img);
-        [~, y] = max(y);
-        if y ~= targets(i)
-            results(1, i) = -1; % missclassified
-            results(2,i) = toc(t);
-            continue;
-        end
-
-        % Check for falsification with upper and lower bounds
-        yUpper = net.evaluate(I.im_ub);
-        [~, yUpper] = max(yUpper);
-        yLower = net.evaluate(I.im_lb);
-        [~, yLower] = max(yLower);
-        if yUpper ~= targets(i) || yLower ~= targets(i)
-            results(1,i) = 0; % not robust
-            results(2,i) = toc(t);
-            continue;
-        end
-
-        % Compute reachability for verification
-        results(1,i) = net.verify_robustness(I, reachOptions, targets(i));
+        results(1,i) = verifySample(net,I,img,target, reachOptions);
         results(2,i) = toc(t);
 
     end
 
 end
 
-% Return an ImageStar of an linf attack
-function I = l_inf_attack(img, epsilon, max_value, min_value)
-    imgSize = size(img);
-    disturbance = epsilon * ones(imgSize, "like", img); % disturbance value
-    lb = max(img - disturbance, min_value);
-    ub = min(img + disturbance, max_value);
-    I = ImageStar(single(lb), single(ub)); % default: single (assume onnx input models)
-end
+
 

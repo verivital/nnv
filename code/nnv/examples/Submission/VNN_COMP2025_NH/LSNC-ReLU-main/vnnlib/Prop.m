03b50f32cec17733c0fa114af053ba635e7bf92e
Navidhashemicodes <navidhashemi.codes@gmail.com>
2025-07-10 17:05:16 -0500

    Non-Necessary files removed

--- code/nnv/examples/Submission/VNN_COMP2025_NH/LSNC-ReLU-main/vnnlib/Prop.m
+++
@@ -1,6 +0,0 @@
-function  [lb, ub, prop] = Prop(vnnlib)
-property = load_vnnlib(vnnlib);
-lb = property.lb; % input lower bounds
-ub = property.ub; % input upper bounds
-prop = property.prop; % output spec to verify
-end

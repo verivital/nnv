d89d2cbf80d5f22e57dc8083f02264d373d48928
m-usama-z <m.usama.zubair978@gmail.com>
2025-11-08 03:00:03 -0600

    adjusted Collins RUL CNN tutorial

--- code/nnv/examples/Tutorial/NN/Collins_RUL/collins_RUL_test.m
+++ code/nnv/examples/Tutorial/NN/Collins_RUL/collins_RUL_test.m
@@ -14,8 +14,6 @@ reachOptions.single_average_input = 1;
 % Select network to verify
 matlab_net = importNetworkFromONNX(nn_path);
 
-fprintf("Network: %s\n", net_name);
-
 % transform into NNV
 net = matlab2nnv(matlab_net);
 net.Name = 'collins_rul_cnn_small_window_20';
@@ -32,34 +30,39 @@ reachOptions.free_mem_frac_for_LPs = 0.1;
 % reachOptions.disp_intersection_result = 1;
 
 % designation of number of workers to use for parallelization; skip the "layer_specific_numCores" field to avoid parallelization within layers altogether
-reachOptions.layer_specific_numCores = dictionary( ...
-    ["start", "ReluLayer", "MaxPooling2DLayer", "end"], ...
-    [  64   ,     64     ,         64         ,  64  ]);	% fix the number of workers to use; "end" refers to verification stage
-
 % reachOptions.layer_specific_numCores = dictionary( ...
 %     ["start", "ReluLayer", "MaxPooling2DLayer", "end"], ...
 %     [  64   ,     -1     ,         -1         ,  -1  ]);	% -1 means, estimate the appropriate number of workers using regression-based polynomial for memory usage estimation for star set LPs
+% reachOptions.layer_specific_numCores = dictionary( ...
+%     ["start", "ReluLayer", "MaxPooling2DLayer", "end"], ...
+%     [  64   ,     64     ,         64         ,  64  ]);	% fix the number of workers to use; "end" refers to verification stage
 
 % Perturb weights
 conv2d_layer_indices = net.get_layer_indices(["Conv2DLayer"]);
 perturbed_layer_index = conv2d_layer_indices(1);
 
-f = 1e-3;	% fraction of layer weight range to use as perturbation
+f = 0.02;	% fraction of layer weight range to use as perturbation
+% At f = 0.02, the property is satisfied
+% At f = 0.05, the property is not satisfied
 
 % copy neural network from blank (unmodified)
 net = getArrayFromByteStream(netbs);
 
-net.Layers{l_index_in_net}.perturb_whole_layer_given_fraction_of_weights_range(f);
+net.Layers{perturbed_layer_index}.perturb_whole_layer_given_fraction_of_weights_range(f);
 
 % Verify network
 t = tic;
-modelstar_result = net.verify_vnnlib(vnnlib_path, reachOptions, needReshape);
-modelstar_time = toc(t);
-if modelstar_result == 2
-    modelstar_result = 0;
+result = net.verify_vnnlib(vnnlib_path, reachOptions, needReshape);
+time = toc(t);
+if result == 2
+    result = 0;
 end
 
-fprintf("m%d(%.3fs) c%d(%.3fs) ", modelstar_result, modelstar_time, cert_robust_result, cert_robust_time);
+if result
+    disp("Property satisfied")
+else
+    disp("Property NOT satisfied")
+end
 
 
 

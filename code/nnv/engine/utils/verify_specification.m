d9fb09473e1791aa3345208a07d5a35b7ae353f1
m-usama-z <m.usama.zubair978@gmail.com>
2025-11-08 02:40:23 -0600

    added a tutorial using Collins RUL CNN

--- code/nnv/engine/utils/verify_specification.m
+++ code/nnv/engine/utils/verify_specification.m
@@ -53,113 +53,117 @@ function result = verify_specification(reachSet, property, reachOptions)
             else
                 R2(k) = R(k);
             end
+            if isa(R2(k).V, 'gpuArray')
+                R2(k) = R2(k).changeDevice('cpu');
+            end
         end
         R = R2;
         
         for k = 1:nr
-            if ~isa(R(k), "Star")
-                R(k) = R(k).toStar;
-            end
-            if isa(R(k).V, 'gpuArray')
-                R(k) = R(k).changeDevice('cpu');
-            end
         end
         
-        disp_stuff = ~isempty(reachOptions) && isfield(reachOptions, 'dis_opt') && strcmp(reachOptions.dis_opt, 'display');
-        
-        result = 1;
-        if disp_stuff
-            fprintf("\n\n");
-        end
-        for k = 1:nr % check every reach set vs OR property
-            f = zeros(1, R(k).nVar, 'like', R(k).V); % objective function
-            pred_lb = R(k).predicate_lb;
-            pred_ub = R(k).predicate_ub;
-            C_const = parallel.pool.Constant(R(k).C);
-            d_const = parallel.pool.Constant(R(k).d);
-            pred_lb_const = parallel.pool.Constant(pred_lb);
-            pred_ub_const = parallel.pool.Constant(pred_ub);
-            f_const = parallel.pool.Constant(f);
-            
-            % poolobj = gcp('nocreate');
-            % if isempty(poolobj)
-            %     max_calls = 1;
-            % else
-            %     max_calls = poolobj.NumWorkers;
-            % end
-            
-            results = nan(np, 1);
-            futures = [];
-            for cp = 1:np
-                while ~isempty(futures) && NN.get_free_mem_frac < free_mem_frac_setting_hack
-                    % disp("Pausing for 0.1 s because free memory " + NN.get_free_mem_B/2^30 + " is less than the desirable " + NN.get_total_mem_B*free_mem_frac_setting_hack/2^30)
-                    pause(0.1);
-                end
-                % fetch some output(s)
-                finished_futures_indices = find(arrayfun(@(f) strcmp(f.State, "finished"), futures));
-                if ~isempty(finished_futures_indices)
-                    [temp_results, finished_cp_nos] = fetchOutputs(futures(finished_futures_indices));
-                    futures(finished_futures_indices) = [];
-                    results(finished_cp_nos) = temp_results;
-                    
-                    if isfield(reachOptions, 'dis_opt') && strcmp(reachOptions.dis_opt, 'display')
-                        fprintf("%s \t ", datetime('now'));
-                        disp("Index: " + finished_cp_nos + ";    Result: " + temp_results);
-                    end
-                    
-                    if ~all(temp_results)
-                        result = 0;
-                        cancel(futures);
-                        break;
+        poolobj = gcp('nocreate');
+        if isempty(poolobj)    % don't parallelize
+
+            cp = 1; % current halfspace we are looking at
+            result = 1; % start assuming property is unsat (no intersection)
+            while cp <= np % multiple halfspaces, which means OR assertion
+                for k = 1:nr % check every reach set vs OR property
+                    Set = R(k);
+                    S = Set.intersectHalfSpace(double(property(cp).G), double(property(cp).g));
+                    if isempty(S)
+                        continue; % does nothing, just need an statement, wanted to make this clear
+                    else
+                        result = 2; %  unknown if approx, sat if exact
+                        return;
                     end
                 end
+                cp = cp+1;
+            end
+
+        else    % parallelize
+
+            disp_stuff = ~isempty(reachOptions) && isfield(reachOptions, 'dis_opt') && strcmp(reachOptions.dis_opt, 'display');
+            
+            result = 1;
+            if disp_stuff
+                fprintf("\n\n");
+            end
+            for k = 1:nr % check every reach set vs OR property
+                f = zeros(1, R(k).nVar, 'like', R(k).V); % objective function
+                pred_lb = R(k).predicate_lb;
+                pred_ub = R(k).predicate_ub;
+                C_const = parallel.pool.Constant(R(k).C);
+                d_const = parallel.pool.Constant(R(k).d);
+                pred_lb_const = parallel.pool.Constant(pred_lb);
+                pred_ub_const = parallel.pool.Constant(pred_ub);
+                f_const = parallel.pool.Constant(f);
                 
-                % if disp_stuff
-                %     fprintf("Free memory before launching cp no. %d: %.2g GB", cp, NN.get_free_mem_B/2^30)
-                % end
-                
-                [C_addition, d_addition] = Star.addition_to_C_d_by_intersection_with_halfspace(R(k), property(cp).G, property(cp).g);
-                
-                new_code = 1;
-                
-                if ~new_code
-                    new_C = vertcat(R(k).C, C_addition);
-                    new_d = vertcat(R(k).d, d_addition);
-                    future = parfeval(@Star.isEmptySet_Static, 2, f, new_C, new_d, pred_lb, pred_ub, cp);
-                else
+                results = nan(np, 1);
+                futures = [];
+                for cp = 1:np
+                    while ~isempty(futures) && NN.get_free_mem_frac < free_mem_frac_setting_hack
+                        % disp("Pausing for 0.1 s because free memory " + NN.get_free_mem_B/2^30 + " is less than the desirable " + NN.get_total_mem_B*free_mem_frac_setting_hack/2^30)
+                        pause(0.1);
+                    end
+                    % fetch some output(s)
+                    finished_futures_indices = find(arrayfun(@(f) strcmp(f.State, "finished"), futures));
+                    if ~isempty(finished_futures_indices)
+                        [temp_results, finished_cp_nos] = fetchOutputs(futures(finished_futures_indices));
+                        futures(finished_futures_indices) = [];
+                        results(finished_cp_nos) = temp_results;
+                        
+                        if isfield(reachOptions, 'dis_opt') && strcmp(reachOptions.dis_opt, 'display')
+                            fprintf("%s \t ", datetime('now'));
+                            disp("Index: " + finished_cp_nos + ";    Result: " + temp_results);
+                        end
+                        
+                        if ~all(temp_results)
+                            result = 0;
+                            cancel(futures);
+                            break;
+                        end
+                    end
+                    
+                    % if disp_stuff
+                    %     fprintf("Free memory before launching cp no. %d: %.2g GB", cp, NN.get_free_mem_B/2^30)
+                    % end
+                    
+                    [C_addition, d_addition] = Star.addition_to_C_d_by_intersection_with_halfspace(R(k), property(cp).G, property(cp).g);
+                    
                     future = parfeval(@isEmptySet_Static_local, 2, f_const, C_const, d_const, pred_lb_const, pred_ub_const, cp, C_addition, d_addition);
+                    
+                    if isempty(futures)
+                        futures = future;
+                    else
+                        futures(end + 1) = future;
+                    end
                 end
                 
-                if isempty(futures)
-                    futures = future;
-                else
-                    futures(end + 1) = future;
+                if result
+                    for m = 1:length(futures)
+                        [completedIdx, temp_result, finished_cp_no] = fetchNext(futures);
+                        results(finished_cp_no) = temp_result;
+                        if isfield(reachOptions, 'dis_opt') && strcmp(reachOptions.dis_opt, 'display')
+                            fprintf("%s \t ", datetime('now'));
+                            fprintf("printing after parfeval launches finished; Index: %d \t Result: %d\n", finished_cp_no, temp_result);
+                        end
+                        if ~temp_result
+                            result = 0;
+                            cancel(futures);
+                            break;
+                        end
+                    end
                 end
-            end
-            
-            if result
-                for m = 1:length(futures)
-                    [completedIdx, temp_result, finished_cp_no] = fetchNext(futures);
-                    results(finished_cp_no) = temp_result;
-                    if isfield(reachOptions, 'dis_opt') && strcmp(reachOptions.dis_opt, 'display')
-                        fprintf("%s \t ", datetime('now'));
-                        fprintf("printing after parfeval launches finished; Index: %d \t Result: %d\n", finished_cp_no, temp_result);
-                    end
-                    if ~temp_result
-                        result = 0;
-                        cancel(futures);
-                        break;
-                    end
+                
+                if ~result
+                    break;
                 end
             end
             
             if ~result
-                break;
+                result = 2;
             end
-        end
-        
-        if ~result
-            result = 2;
         end
     end
 
